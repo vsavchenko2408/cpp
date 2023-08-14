ab796a87e83cb343ab4021c6f5a3db3618a69215
1985DerMeister1985 <1985.der.meister.1985@gmail.com>
2023-08-14 16:03:23 +0300

    "sync"

--- cpp/other/other/other.cpp
+++ cpp/other/other/other.cpp
@@ -1,8 +1,48 @@
 ﻿#include <iostream>
+#define  ui unsigned int
+
 using namespace std;
+void get_nod(ui* a, ui* b)
+{
+    cout << "Введите первое число НОД которого хотите вычислить: " << endl;
+    cin >> *a;
+    cout << "Введите второе число НОД которого хотите вычислить: " << endl;
+    cin >> *b;
+    if (*a > *b)
+    {
+        *a = *a - *b;
+    }
+    else{
+        *a = *b - *a;
+        }
+}
+void find_nod(ui* a, ui* b)
+{
+  
+    if (*a % *b == 0)
+    {
+        cout << *b << endl;
+    }   
+    if (*b % *a == 0)
+    {
+        cout << *a << endl;
+    }
+    if (*a > *b)
+        {
+         cout << *a % *b << endl;
+        }
+    if (*a < *b)
+    {
+        cout << *b % *a << endl;
+    }
+}
 int main()
 {
-    cout << "Hello World!" << endl;
+    setlocale(LC_ALL, "RU");
+    ui a,b = 0;
+    get_nod(&a,&b);
+    find_nod(&a, &b);
+
     return 0;
 }
 
