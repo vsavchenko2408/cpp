1c82d41809c6e0da18d5c3f6966679a5bde468fc
DerMeister <1985.der.meister.1985@gmail.com>
2023-08-14 22:35:18 +0300

    sync

--- cpp/other/other/other.cpp
+++ cpp/other/other/other.cpp
@@ -1,47 +1,13 @@
 ﻿#include <iostream>
-#define  ui unsigned int
-
+#include <random>
 using namespace std;
-void get_nod(ui* a, ui* b)
-{
-    cout << "Введите первое число НОД которого хотите вычислить: " << endl;
-    cin >> *a;
-    cout << "Введите второе число НОД которого хотите вычислить: " << endl;
-    cin >> *b;
-    if (*a > *b)
-    {
-        *a = *a - *b;
-    }
-    else{
-        *a = *b - *a;
-        }
-}
-void find_nod(ui* a, ui* b)
-{
-  
-    if (*a % *b == 0)
-    {
-        cout << *b << endl;
-    }   
-    if (*b % *a == 0)
-    {
-        cout << *a << endl;
-    }
-    if (*a > *b)
-        {
-         cout << *a % *b << endl;
-        }
-    if (*a < *b)
-    {
-        cout << *b % *a << endl;
-    }
-}
+
 int main()
 {
     setlocale(LC_ALL, "RU");
-    ui a,b = 0;
-    get_nod(&a,&b);
-    find_nod(&a, &b);
+    srand
+    cout << "\t\tИгра камень-ножницы-бумага" << endl;
+    cout << "-------------------------------------------------------------------------" << endl;
 
     return 0;
 }
