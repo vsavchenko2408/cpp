b25e8f37fb8bc5e6af7997cbe2b54523f1bbd005
DerMeister <1985.der.meister.1985@gmail.com>
2023-08-17 16:15:54 +0300

    "edit bancacc"

--- BankAcc/BankAcc/BankAcc.cpp
+++ BankAcc/BankAcc/BankAcc.cpp
@@ -10,11 +10,17 @@ typedef unsigned int ui;
 using namespace std;
 class banacc
 {
+public:
 	banacc()
 	{
+		cout << "Enter ID :" << endl;
+		cin >> id_acc;
+		cout << " Enter your name : " << endl;
+		cin >> name;
+		
 		money = 0;
 	}
-public:
+	
 	void show()
 	{
 		cout << "------------------------------------------------------------------" << endl;
@@ -23,12 +29,27 @@ public:
 		cout << "---" << "Amount money: " << money;
 		cout << "------------------------------------------------------------------" << endl;
 	}
-	void add()
+	void addmoney()
 	{
 		ui add=0;
 		cout << "Enter amount: " << endl;
 		cin >> add;
 		money += add;
+	}
+	void take()
+	{
+		
+		ui take = 0;
+		cout << "How money you want take: " << endl;
+		cin >> take;
+		if (money < take)
+		{
+			cout << "Not enough money" << endl;
+		}
+		else 
+		{
+			money -= take;
+		}
 	}
 
 
@@ -41,9 +62,62 @@ private:
 
 int main()
 {
+	int code;
+	cout << "\tWelcome!" << endl;
+	cout << "If you want exit enter \"exit\" " << endl;
+	do {
+		cout << "Created acc enter 1: " << endl;
+		cout << "Add money enter 2:" << endl;
+		cout << "Show info from account 3: " << endl;
+		cin >> code;
+		switch (code)
+		{
+		case 1:
+			banacc a;
+			break;
+		case 2:
+			a.addmoney();
+			break;
+		case 3:
+			a.show();
+			break;
+		case 4:
+
+		case 5:
+
+		case 6:
+
+		default:
+		}
+
+
+		banacc a;
+		a.addmoney();
+		a.show();
+		a.take();
+		a.show();
+
+		banacc b;
+		b.addmoney();
+		b.show();
+		b.take();
+		b.show();
+
+	} while (!exit);
 
 
 
+	banacc a;
+	a.addmoney();
+	a.show();
+	a.take();
+	a.show();
+
+	banacc b;
+	b.addmoney();
+	b.show();
+	b.take();
+	b.show();
 
 	return 0;
 }
