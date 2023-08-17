f1d9a42c1a5d6caacf19161e638d5f40a7c70a52
DerMeister <1985.der.meister.1985@gmail.com>
2023-08-17 22:17:30 +0300

    edit banacc

--- BankAcc/BankAcc/BankAcc.cpp
+++ BankAcc/BankAcc/BankAcc.cpp
@@ -62,62 +62,15 @@ private:
 
 int main()
 {
-	int code;
-	cout << "\tWelcome!" << endl;
-	cout << "If you want exit enter \"exit\" " << endl;
-	do {
-		cout << "Created acc enter 1: " << endl;
-		cout << "Add money enter 2:" << endl;
-		cout << "Show info from account 3: " << endl;
-		cin >> code;
-		switch (code)
-		{
-		case 1:
-			banacc a;
-			break;
-		case 2:
-			a.addmoney();
-			break;
-		case 3:
-			a.show();
-			break;
-		case 4:
-
-		case 5:
-
-		case 6:
-
-		default:
-		}
-
-
-		banacc a;
+	banacc a;	
 		a.addmoney();
 		a.show();
 		a.take();
 		a.show();
-
 		banacc b;
 		b.addmoney();
 		b.show();
 		b.take();
 		b.show();
-
-	} while (!exit);
-
-
-
-	banacc a;
-	a.addmoney();
-	a.show();
-	a.take();
-	a.show();
-
-	banacc b;
-	b.addmoney();
-	b.show();
-	b.take();
-	b.show();
-
 	return 0;
 }
