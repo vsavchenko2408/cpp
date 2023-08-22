50a84792930ae40ed6abed27f1299f1e6c6c2e49
1985DerMeister1985 <1985.der.meister.1985@gmail.com>
2023-08-22 16:38:16 +0300

    add queue

---
+++ cpp/queue/queue/queue.cpp
@@ -0,0 +1,37 @@
+﻿/*
+Задание 3:
+Реализуйте класс Stack, представляющий стек.
+Стек должен иметь методы для добавления элемента на вершину стека,
+удаления элемента с вершины стека и получения значения верхнего элемента без его удаления.
+*/
+
+#include <queue>
+#include <iostream>
+#include<stack>
+using namespace std;
+class mystack
+{
+private:
+    int* arr;
+    int lng;
+    int indx;
+public:
+    mystack(int lng)
+    {
+        indx = 0;
+        arr = new int[lng];
+    };
+    ~mystack()
+    {
+        delete[] arr;
+    };
+    
+};
+int main()
+{
+
+    
+ 
+    return 0;
+}
+
