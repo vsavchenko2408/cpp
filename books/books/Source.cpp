0aa597a09d5cd1ab6875886cc96cbe726216550b
DerMeister <1985.der.meister.1985@gmail.com>
2023-10-11 21:56:37 +0300

    books and library

--- books/books/Source.cpp
+++ books/books/Source.cpp
@@ -3,12 +3,29 @@
 #include <string>
 #include <fstream>
 
+
 using namespace std;
 
 class book
 {
 public:
+	book()
+	{
 
+	}
+	book()
+	{
+		cout << "Введите название книги: ";
+		cin >> name;
+		cout << "Введите имя автора: ";
+		cin >> author;
+		cout << "Введите год издания:";
+		cin >> year;
+	}
+	~book()
+	{
+		cout << "Книга удалена!" << endl;
+	}
 private:
 	string name;
 	string author;
@@ -18,9 +35,47 @@ private:
 class library
 {
 public:
+	void add_book(book)
+	{
 
+	}
+	void remove_book(book)
+	{
+
+	}
+	//void find_book(author)
+	
+	//void find_book(year)
+
+	void list_book()
+	{}
 private:
 
 	list<string> books;
 
 };
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	ofstream out;
+	book first;
+	out.open("Library.txt");
+	if (!out.is_open())
+	{
+		cout << "Ошибка открытия файла!" << endl;
+	}
+	else
+	{
+		out.write((char*)&first,sizeof(book));
+		cout << "Запись в файл успешна. " << endl;
+	}
+
+	out.close();
+	
+
+
+
+
+
+}
