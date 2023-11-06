6e870442bb95ecf85bd90323f095215cc4f89f06
DerMeister <1985.der.meister.1985@gmail.com>
2023-11-06 03:23:19 +0200

    sync

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using std::vector;
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+    vector<int> MyVec;
+    MyVec.push_back(15);
+    MyVec.push_back(40);
+    MyVec.push_back(34);
+    MyVec.push_back(67);
+    MyVec.push_back(3);
+    sort(MyVec.begin(), MyVec.end());
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << MyVec.at(i) << endl;
+    }
+    return 0;
+}
