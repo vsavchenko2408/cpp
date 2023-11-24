4941625259b79cd7d4cd211348f28bbae420f7f0
1985DerMeister1985 <1985.der.meister.1985@gmail.com>
2023-11-25 00:00:41 +0200

    sync

--- CBproject/main.cpp
+++ CBproject/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "MyClass.h"
+
 using std::cout;
 using std::cin;
 using std::endl;
@@ -14,14 +14,7 @@ return b;
 
 int main()
 {
-/*
-MyClass obj1;
-obj1.set_data(14,25);
-obj1.get_data();
-MyClass obj2;
-obj2.set_data(1,5);
-obj2.get_data();
-*/
+
 cout << MyFoo('F',400);
     return 0;
 }
