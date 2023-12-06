635c3241f36b4d6184d1907129171c6fb8560662
DerMeister1985 <1985.der.meister.1985@gmail.com>
2023-12-06 08:34:41 +0200

    created taskmanager

---
+++ Taskmanager/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+class Tasks
+{
+    public:
+void new_task(std::string nm,bool st)
+{
+name = nm;
+status = st;
+}
+
+
+
+
+private:
+std::string name;
+bool status;
+};
+
+
+
+
+
+
+
+int main()
+{
+    std::cout << "Everything is OK!" << std::endl;
+    return 0;
+}
