ca6f8c5a0ae2f20d74109a368645d5fbe28efbf3
DerMeister1985 <1985.der.meister.1985@gmail.com>
2023-12-07 11:38:43 +0200

    tasks

--- Taskmanager/main.cpp
+++ Taskmanager/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 
+
 using std::cout;
 using std::endl;
 using std::cin;
 using std::vector;
+
 
 
 class Tasks
@@ -16,11 +18,26 @@ class Tasks
         cin >> name;
         status = false;
     }
+    ~Tasks()
+    {
+    cout << "Destructor" << endl;
+    }
 void new_task(std::string nm,bool st)
     {
     name = nm;
     status = st;
     }
+void complete_task()
+{
+    if(!status)
+    status = true;
+    else
+    cout << "Task was complete! " << endl;
+}
+void del_task()
+{
+    Tasks::~Tasks();
+}
 void show_tasks()
     {
     cout << "Task : " << name << endl;
@@ -40,8 +57,18 @@ bool status;
 int main()
 {
     vector<Tasks> VecTask;
+
+
+
+
+    
+    /*
 VecTask.push_back(Tasks());
+VecTask[0].complete_task();
+VecTask[0].show_tasks();
+VecTask[0].del_task();
 //VecTask.push_back(Tasks::new_task("Buy food", false));
     std::cout << "Everything is OK!" << std::endl;
+    */
     return 0;
 }
