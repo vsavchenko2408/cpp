07f8fe555539e8e221b60a469225142ace31174b
DerMeister1985 <1985.der.meister.1985@gmail.com>
2023-12-15 15:29:22 +0200

    strlen

---
+++ strlenforchatGPT/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using std::cout,std::cin,std::endl, std::string;
+
+int main()
+{
+string sometext;
+cout << "Введите текст: " << endl;
+cin >> sometext;
+
+
+
+    return 0;
+}
