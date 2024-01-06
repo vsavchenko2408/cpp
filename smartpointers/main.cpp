c91f5c238c99193bafc8623d1813027699d093e9
DerMeister <1985.der.meister.1985@gmail.com>
2024-01-06 11:44:42 +0200

    smartpointers

--- smartpointers/main.cpp
+++ smartpointers/main.cpp
@@ -2,17 +2,36 @@
 using std::cout;
 using std::cin;
 using std::endl;
-
+template<typename T>
 class SmartPointer
 {
+public:
+SmartPointer(T *ptr)
+{
+this->ptr = ptr;
+cout << "Constructor " << endl;
+}
 
+~SmartPointer()
+{
+    delete ptr;
+    cout << "Destructor " << endl;
+}
+
+T& operator *()
+{
+    return *ptr;
+}
+
+private:
+T *ptr;
 
 };
 
 int main()
 {
-
-
+SmartPointer<int> pointer = new int(5);
+cout << *pointer << endl;
 
 
     return 0;
