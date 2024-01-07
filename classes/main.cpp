7830e9cde34be7393d71e4ad5d94bb1b40f5265e
DerMeister <1985.der.meister.1985@gmail.com>
2024-01-07 21:21:20 +0200

    classes

--- classes/main.cpp
+++ classes/main.cpp
@@ -23,10 +23,10 @@ void show()
     cout << "X = " << *xptr << endl;
     cout << "Y = " << *yptr << endl;
 }
-pointers operator == (const pointers &)
+
+bool operator==(const pointers &other) const
 {
-if(this->xptr < other->xptr||this->xptr > other->xptr||this->yptr > other->yptr||this->yptr < other->yptr){return false;}
-else{return true;}
+    if (*this->xptr == *other.xptr && *this->yptr == *other.yptr) {return true;} else{return false;}
 }
 
 
@@ -42,5 +42,9 @@ int main()
     pointers second(5,6);
     first.show();
     second.show();
+    
+    if(bool dif = first == second) cout << "True"<< endl;
+    else cout << "False" << endl;
+
     return 0;
 }
