8e335fe6b77bf8f7a860f2dae37bebda29c5137d
DerMeister <1985.der.meister.1985@gmail.com>
2024-01-07 21:07:06 +0200

    classes

--- classes/main.cpp
+++ classes/main.cpp
@@ -8,23 +8,39 @@ using std::vector;
 class pointers
 {
 public:
-pointers()
+pointers(int x, int y)
 {
-
+this->xptr = new int(x);
+this->yptr = new int(y);
 }
 ~pointers()
 {
 delete xptr;
 delete yptr;
 }
+void show()
+{
+    cout << "X = " << *xptr << endl;
+    cout << "Y = " << *yptr << endl;
+}
+pointers operator == (const pointers &)
+{
+if(this->xptr < other->xptr||this->xptr > other->xptr||this->yptr > other->yptr||this->yptr < other->yptr){return false;}
+else{return true;}
+}
+
 
 private:
-int *xptr = new int;
-int *yptr = new int;
+    int* xptr;
+    int* yptr;
 };
 
 
 int main()
 {
+    pointers first(3,6);
+    pointers second(5,6);
+    first.show();
+    second.show();
     return 0;
 }
