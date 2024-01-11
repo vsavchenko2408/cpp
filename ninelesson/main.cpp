93049e5367c34a115eee8be0d3280dfce34e92b1
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-11 15:23:17 +0200

    ninelesson

--- ninelesson/main.cpp
+++ ninelesson/main.cpp
@@ -3,9 +3,24 @@
 using std::cout;
 using std::endl;
 
+//функция сложения
+int AddictionFunc(int d, int e)
+{
+return d+e;
+}
+//функция вывода
+void ShowFunc(int e)
+{
+    cout << "Result is: " << e << endl;
+   
+}
 int main()
 {
-   
+ int a = 5; 
+ int b = 3; 
+ int f = AddictionFunc(a,b);
+
+ShowFunc(f);
 
     return 0;
 }
