43a87f1b8e50916d4077e587509f18a29b0268ec
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-13 00:21:05 +0200

    arrays

--- arrays/main.cpp
+++ arrays/main.cpp
@@ -3,6 +3,24 @@ using namespace std;
 
 int main()
 {
- 
+/*
+const int N=10;
+int arr[N];
+for(int i=0;i<N;i++)
+{
+arr[i]=i;
+cout << arr[i] << endl;
+}
+*/
+
+short arr[] = {1,2,3,4};
+for(int i=0;i<8;i++)
+{
+    
+cout << arr[i] = 0 ;
+}
+
+
+
     return 0;
 }
