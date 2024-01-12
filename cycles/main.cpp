9e7bcfe9276facde855f07c509d47b651f7f43ea
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-12 23:23:52 +0200

    cycles

--- cycles/main.cpp
+++ cycles/main.cpp
@@ -6,6 +6,38 @@ using std::endl;
 
 int main()
 {
+ /*   int choise;
 
+    do
+    {
+        cin >> choise;
+    switch(choise)
+    {
+case 1:
+    cout << "Choise 1 " << endl;
+    break;
+case 2:
+    cout << "Choise 2 " << endl;
+    break;
+case 3:
+    cout << "Choise 3 " << endl;
+    break;
+default:
+    cout << "Error choise" << endl;
+    continue;
+}
+break;
+}  while(choise);
+*/
+
+for(int i=0;i<10;i++)
+{
+    for(int j=0; j<10;j++)
+    {
+        cout << j;
+    }   
+cout << " " << i  << endl;
+}
+    
     return 0;
 }
