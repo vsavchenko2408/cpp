a5282d1dea4de91841c44a93cd594b13e53c7922
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-14 21:42:57 +0200

    typecasting

---
+++ typecasting/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+
+float f = 3.9f;
+float i = static_cast<double>(f)+4.2;
+cout << i << endl;
+
+
+
+
+    return 0;
+}
