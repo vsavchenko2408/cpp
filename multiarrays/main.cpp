43ec7f340ef8ae9a2c365720751d8e7869529dae
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-16 02:17:30 +0200

    edit

--- multiarrays/main.cpp
+++ multiarrays/main.cpp
@@ -4,16 +4,13 @@
 
 int main()
 {
-int arr[5][5][5];
+int arr[5][5];
 
 for(int i=0;i<5;i++)
 {
     for(int j=0;j<5;j++)
     {
-        for(int k=0;k<5;k++)
-        {
-        arr[i][j][k] = j;
-        }
+        arr[i][j] = j+1;
     }
 }
 
@@ -21,11 +18,7 @@ for(int i=0;i<5;i++)
 {
     for(int j=0;j<5;j++)
     {
-        for(int k=0;k<5;k++)
-        {
-      cout <<  arr[i][j][k];
-        }
-        cout << endl;
+      cout <<  arr[i][j]; 
     }
     cout << endl;
 }
