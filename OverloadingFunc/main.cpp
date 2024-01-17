d64757c4caeef698f052f1afe2814eb535812125
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-17 19:28:11 +0200

    edit OverloadingFunc

--- OverloadingFunc/main.cpp
+++ OverloadingFunc/main.cpp
@@ -1,20 +1,7 @@
 #include <iostream>
 using namespace std;
-int foo(int a)
-{
-    return a+10;
-}
 
-float foo(float a)
-{
-    return a+10.0;
-}
 int main()
-{
-int a = 5;
-float f = 5.2;
-cout << foo(f) << endl;
-
 
     return 0;
 }
