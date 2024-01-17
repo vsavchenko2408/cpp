81da3c26508d01d26ee1bb455ccd126d5683e0dc
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-17 21:36:42 +0200

    sync

--- OverloadingFunc/main.cpp
+++ OverloadingFunc/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 using namespace std;
 
+double foo(double a, double b)
+{
+    cout << "Double" << endl;
+    return a * b;
+    
+}
+int foo(int a)
+{
+    cout << "Integer" << endl;
+    return a * a;
+    
+}
+
+
 int main()
 {
+    cout << foo(5) << endl;
+    cout << "######################################" << endl;
+    cout << foo(5.2f) << endl;
+    cout << "######################################" << endl;
+    cout << foo(5, 6) << endl;
+    cout << "######################################" << endl;
     return 0;
 }
