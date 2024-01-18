d336736ae5d60446d6b3763cdc4cda4f0875c21f
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-18 23:03:06 +0200

    minigame

--- minigame/main.cpp
+++ minigame/main.cpp
@@ -4,6 +4,40 @@ using std::endl;
 
 int main()
 {
- 
+char step;
+int x=0;
+int y=0;
+cout << "\tMiniGame." << endl;
+
+while(step != '\r')
+{
+cout << "Your coordinates: " << x << '.' << y << endl;
+cout << "Tap enter to exit " << endl;
+cout << "Input step(w,a,s,d): " ;
+step = getchar();
+switch(step)
+{
+case 'w':
+    y++;
+    break;
+case 's':
+    y--;
+    break;
+case 'a':
+    x--;
+    break;
+case 'd':
+    x++;
+    break;
+case '\r':
+    cout << "Goodbye"<< endl;
+    exit(step);
+    break;
+
+default:
+cout << "Error input, try again " << endl;
+}
+}
+
 return 0;
 }
