f31504a4b82dbb8e21983a06ba6992dd64fb139a
DerMeister <1985.der.meister.1985@gmail.com>
2024-01-18 21:45:21 +0200

    adventure

---
+++ adventure/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+using std::cout;
+using std::endl;
+using std::cin;
+int main()
+{
+char step;
+int x = 0;
+int y = 0;
+cout << "Enter for exit " << endl;
+while(step != '\r')
+{
+    cout << "Your coordinates : " << x << ", " << y << endl;
+    cout << "Input side (w.a.s.d) " << endl;
+    step = _getche();
+
+    switch(step)
+    {
+        case 'w':
+        y++;
+        break;
+        case 's':
+        y--;
+        break;
+        case 'a':
+        x--;
+        break;
+        case 'd':
+        x++;
+        break;
+        case '\r':
+        exit();
+        default:
+        cout << "Error input, try again " << endl;
+    }
+}
+    return 0;
+}
