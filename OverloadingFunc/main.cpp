ed77ef81bb4f83b5c8cbf005b72faef98a42097a
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-19 20:44:59 +0200

    OverloadingFunc

--- OverloadingFunc/main.cpp
+++ OverloadingFunc/main.cpp
@@ -1,27 +1,14 @@
 #include <iostream>
 using namespace std;
 
-double foo(double a, double b)
+template<class T1, class T2>
+T1 foo(T1 a, T2 b)
 {
-    cout << "Double" << endl;
-    return a * b;
-    
+    cout << a << b << endl;
 }
-int foo(int a)
-{
-    cout << "Integer" << endl;
-    return a * a;
-    
-}
-
-
 int main()
 {
-    cout << foo(5) << endl;
-    cout << "######################################" << endl;
-    cout << foo(5.2f) << endl;
-    cout << "######################################" << endl;
-    cout << foo(5, 6) << endl;
-    cout << "######################################" << endl;
+foo( 5.2, " string");
+
     return 0;
 }
