1f6f924e1f7c51da06c50603a0927bf539fa2613
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-21 01:07:29 +0200

    logicoper

---
+++ logicoper/main.cpp
@@ -0,0 +1,33 @@
+/*
+Логические операторы
+
+&& - и
+|| - или
+!= - не
+
+*/
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+
+bool a = false;
+bool b = true;
+bool c = false;
+
+if(!a||b)
+{
+    cout << " true "<< endl;
+}
+else
+cout << " false " << endl;
+
+
+
+
+
+return 0;
+}
