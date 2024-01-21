5d68f460152c6fb3d27cb8b0871de28cdf4cca2b
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-21 19:01:23 +0200

    edit Rec

--- Rec/main.cpp
+++ Rec/main.cpp
@@ -1,24 +1,10 @@
 #include <iostream>
 using namespace std;
-static int i=0;
 
-void foo()
-{
-    i++;
-    cout << i << endl;
-    if(i == 100)
-    {
-    return;
-    }
-    else
-    {
-    foo();
-        }
-}
 int main()
 {
 
-foo();
-cout << sizeof(foo());
+
+
     return 0;
 }
