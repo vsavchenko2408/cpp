a22300ffcbc8b6d7abc54a37129243230f2b8d70
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-21 20:35:42 +0200

    Rec

--- Rec/main.cpp
+++ Rec/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int foo(int i)//рекурсивная функция
+{
+    i++;
+    if(i<=5)
+    {
+        cout << "Enter recursive foo " << i << endl;
+        foo(i);// Вход в рекурсивную функцию
+        cout << "Exit recursive foo :" << i << endl; //выход из рекурсии
+    }  
+    return i; 
+}
 int main()
 {
-
-
-
+int i = 0;  
+int b = foo(i);
+cout << "B = " << b << endl;
     return 0;
 }
