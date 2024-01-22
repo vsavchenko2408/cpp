5d81c3acbb7a366691e225d7d04464aefd56b8e6
DerMeister <1985.der.meister.1985@gmail.com>
2024-01-22 22:41:27 +0200

    MinigameVStudio

--- MinigameVStudio/MinigameVStudio/MinigameVStudio.cpp
+++ MinigameVStudio/MinigameVStudio/MinigameVStudio.cpp
@@ -1,27 +1,41 @@
 ﻿
 #include <iostream>
-using namespace std;
 #include <conio.h>
+using std::cout;
+using std::endl;
+using std::cin;
 int main()
 {
-    setlocale(LC_ALL, "RU");
-    char dir = 'a';
-    int x = 10, y = 10;
-    while (dir != '\r')
+    char step = ' ';
+    int x = 0;
+    int y = 0;
+    cout << "Enter for exit " << endl;
+    while (step != '\r')
     {
-        cout << "\nВаши координаты: " << x << ", " << y;
-        cout << "\nВыберите направление (w, s, d, a): ";
-        dir = _getche();
-        switch (dir) 
+        cout << "Your coordinates : " << x << ", " << y << endl;
+        cout << "Input side (w.a.s.d) " << endl;
+        step = _getche();
+
+        switch (step)
         {
-        case 'w': y--; break;
-        case 's': y++; break; 
-        case 'd': x++; break; 
-        case 'a': x--; break; 
-        case '\r': cout << "Выход...\n"; break; 
-        default: cout << "Попробуйте еще\n";
-        } 
+        case 'w':
+            y++;
+            break;
+        case 's':
+            y--;
+            break;
+        case 'a':
+            x--;
+            break;
+        case 'd':
+            x++;
+            break;
+        case '\r':
+            break;
+        default:
+            cout << "Error input, try again " << endl;
+        }
         system("CLS");
-    } 
+    }
     return 0;
 }
