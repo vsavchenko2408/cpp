1890f0d16c7a62d0fc69250a5b3ccab1bfe859d1
DerMeister <1985.der.meister.1985@gmail.com>
2024-01-23 02:23:40 +0200

    pointers

--- cpp/Pointers/Pointers/Pointers.cpp
+++ cpp/Pointers/Pointers/Pointers.cpp
@@ -2,19 +2,36 @@
 Указатель: переменная которая содержит адрес переменной
 тип* имя - указатель = & - взятие адреса переменной
 
-Ссылка - псевдоним другого объекта или знаения
+Ссылка - псевдоним другого объекта или значения
 тип &имя = объект
 */
 #include <iostream>
+#include <random>
 using std::cout;
 using std::cin;
 using std::endl;
 
+void fillarray(int* arr, const int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = rand();
+    }
+}
+
 int main()
 {
-    int a = 15;
-    cout << a << endl;
+    srand(time(NULL));
+    const int N = 10;
+    int arr[N];
+    fillarray(arr, N);
 
+    for (int i = 0; i < N; i++)
+    {
+        cout << "Arr: " << i+1 << " = " << arr[i] << endl;
+        cout << "Address: " << i+1 << " = " << &arr[i] << endl;
+    }
+  
     return 0;
 }
 
