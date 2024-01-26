6fba6bf74b63bb0f1b6b92ea40555a2b76c7446d
DerMeister <1985.der.meister.1985@gmail.com>
2024-01-26 09:26:31 +0200

    Game

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "player.h"
 using std::cout;
 using std::cin;
 using std::endl;
--- Game/map.cpp
+++ Game/map.cpp
@@ -13,8 +13,21 @@ Map()
 
 
 }
-
-
+void draw_map()
+{
+for(int i = 0;i<VER;i++)
+    {
+    for(int j = 0;i<GOR;j++)
+        {
+            cout << "#" ;
+        } 
+        cout << "\n";
+    }
+}
 private:
+const int VER = 10;
+const int GOR = 10;
+char arrmap[][];
+
 
 };
