20ea63bcf09ad4bb3345da0514f67c9823526e9b
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-27 12:28:03 +0200

    Game

--- Game/player.cpp
+++ Game/player.cpp
@@ -1,63 +1,56 @@
+#pragma once 
 #include <iostream>
-
-#pragma once
-
 class Player
 {
-    Player(unsigned short coor_x = 5,unsigned short coor_y = 5,unsigned short health = 100, unsigned short armour = 0, unsigned short ammo = 20)
-    {
-        this->coor_x = coor_x;
-        this->coor_y = coor_y;
-        this->health = health;
-        this->armour = armour;
-        this->ammo = ammo;
-    }
 public:
-void player_move(unsigned short coor_x, unsigned short coor_y)
+Player(int health, int armour, int ammo)
 {
-if(coor_x > this->coor_x) this->coor_x++;
-else if(coor_x < this->coor_x) this->coor_x--;
-else if(coor_y > this->coor_y) this->coor_y++;
-else if(coor_y < this->coor_y) this->coor_y--;
+this->health = health;
+this->armour = armour;
+this->ammo = ammo;
+x=5;
+y=5;
+}
+void show_info()
+{
+std::cout << "Health: " << health << std::endl;
+std::cout << "Armour: " << armour << std::endl;
+std::cout << "Anmo: " << ammo << std::endl;
+}
+void set_coor(char move)
+{
+switch(move)
+{
+case 'w':
+    this->y++;
+    break;
+case 's':
+    this->y--;
+    break;
+case 'a':
+    this->x--;
+    break;
+case 'd':
+    this->x++;
+    break;
+default:
+    std::cout << "Wrong input" << std::endl;
+}    
+}
+void get_coor()
+{
+std::cout << "X: " << this->x << std::endl;
+std::cout << "Y: " << this->y << std::endl;
+}
+~Player()
+{
+    
+}
 
-}
-void heal_player(int val)
-{
-    if(this-> health < 100)
-    {    
-    this->health = health+val;
-    }
-    else
-    {
-        std::cerr << "Health is Full! " << std::endl;
-    }
-}
-void armour_player(int val)
-{
-   if(this-> armour < 100)
-    {    
-    this->armour = armour+val;
-    }
-    else
-    {
-        std::cerr << "Armour is Full! " << std::endl;
-    }  
-}
-void ammo_player(int val)
-{
-   if(this-> ammo < 255)
-    {    
-    this->ammo = armour+val;
-    }
-    else
-    {
-        std::cerr << "Ammo is Full! " << std::endl;
-    }  
-}
 private:
-unsigned short coor_x;
-unsigned short coor_y;
-unsigned short health;
-unsigned short armour;
-unsigned short ammo;
+short x;
+short y;
+int health;
+int armour;
+int ammo;
 };
