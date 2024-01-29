affa8bd5c6574c3cfac7de83dee4c0e5511e845d
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-29 22:40:43 +0200

    Enum

--- Enum/main.cpp
+++ Enum/main.cpp
@@ -1,11 +1,19 @@
-
 #include <iostream>
 using std::cout;
 using std::cin;
 using std::endl;
+class MyClass
+{
+public:
+int y;
+private:
+int x;
+};
 
 int main()
 {
+    MyClass MyCl;
+    MyCl::MyCl.y = 10;
 
     return 0;
 }
