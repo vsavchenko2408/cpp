34e38961279ceb493f04f8861abea94f47062116
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-30 02:21:46 +0200

    Enum

--- Enum/main.cpp
+++ Enum/main.cpp
@@ -3,19 +3,21 @@
 using std::cout;
 using std::cin;
 using std::endl;
-enum Days
+union UN
 {
-    monday,
-    tuesday,
-    wednesday
-};
+  int i;
+  float f;
+  double d;
+  char c;
+} un1,un2;
 
 int main()
 {
-    Days week;
-    week = Days::monday;
+    un1.i = 5;
 
-
-
+    cout << "i: " << un1.i << endl;
+    cout << "f: " << un1.f << endl;
+    cout << "d: " << un1.d << endl;
+    cout << "c: " << un1.c << endl;
     return 0;
 }
