5f5f1ffd997c149411e9fcabd8dac85522c43dd3
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-30 04:34:53 +0200

    Enum

--- Enum/main.cpp
+++ Enum/main.cpp
@@ -4,8 +4,26 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+union UN
+{
+   int i;
+   float f;
+   double d;
+   char c;
+};
+
 int main()
 {
-   
+    UN MyUn;
+    MyUn.i = 100;
+   cout << "i = " << MyUn.i << endl;
+   cout << "f = " << MyUn.f << endl;
+   cout << "d = " << MyUn.d << endl;
+   cout << "c = " << MyUn.c << endl;
+   MyUn.d = 4.2;
+   cout << "i = " << MyUn.i << endl;
+   cout << "f = " << MyUn.f << endl;
+   cout << "d = " << MyUn.d << endl;
+   cout << "c = " << MyUn.c << endl;
     return 0;
 }
