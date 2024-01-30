118c4d55f927b08d34623b5eace8115f2677a070
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-30 20:38:42 +0200

    struct

--- struct/main.cpp
+++ struct/main.cpp
@@ -1,12 +1,61 @@
 #include <iostream>
+#include <string>
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
+struct coor
+{
+    void showcoor()
+    {
+        cout << "X = " << x; 
+        cout << " Y = " << y << endl;
+    }
+int x;
+int y;
+};
+struct human
+{
+void info()
+{
+cout << "Name: " << name << endl;
+cout << "Age: " << age << endl;
+cout << "Weight: " << weight << endl;
+cout << "Height: " << height << endl;
+cout << "ID:" << id << endl; 
+cout << "Coordinades: ";
+xy.showcoor();
+}
 
+
+string name;
+unsigned int age;
+unsigned int weight;
+unsigned int height;
+int id = 100;
+coor xy;
+};
 
 int main()
 {
 
+human man;
+man.name = "Ivan";
+man.age = 19;
+man.height = 180;
+man.weight = 90;
+man.xy.x = 5;
+man.xy.y = 6;
+
+man.info();
+cout << endl;
+cout << endl;
+human woman;
+woman.name = "Maria";
+woman.age = 20;
+woman.height = 165;
+woman.weight = 55;
+woman.info();
 
     return 0;
 }
