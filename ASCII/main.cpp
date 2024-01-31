91851467553abab4f67dcb65016c061c4e105650
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-01-31 19:39:18 +0200

    ASCII

--- ASCII/main.cpp
+++ ASCII/main.cpp
@@ -5,6 +5,14 @@ using std::endl;
 
 int main()
 {
+char c;
+for(int i = 1; i< 128;i++)
+{
+c++;
+
+cout << "Code: "  << std::dec << i  << " " << std::oct << i << " " << std::hex << i <<" = " << c << endl;
+
+}
 
     return 0;
 }
