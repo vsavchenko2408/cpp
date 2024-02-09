b4252f702ccb973f0aa0f3864fdbfc9e18b05386
DerMeister <1985.der.meister.1985@gmail.com>
2024-02-09 12:36:03 +0200

    files

--- files/main.cpp
+++ files/main.cpp
@@ -8,30 +8,52 @@ int main()
 {
 srand(time_t(NULL));
 const int N = 100;
-int* arr = new int[N];
+int* arr_out = new int[N];
 
-std::ofstream myfile("config.txt", std::ios_base::in);
+std::ofstream myfile("config.txt", std::ios_base::out);
 //myfile.open("config.txt");
-//std::ofstream myfile("config.txt");
-/*if(myfile.is_open())
+
+if(myfile.is_open())
     {
         cout << "File open!" << endl;
         //myfile << "Test" << endl;
     for(int i=0;i<N;i++)
         {
-        arr[i] = rand()%999;
+        arr_out[i] = rand()%999;
        
-        myfile << arr[i] << endl;
+        myfile << arr_out[i] << endl;
         }
     }
 else
 {
    cout << "File is not open! " << endl;
 }
-*/
+delete[] arr_out;
 myfile.close();
-cout << "File is close " << endl;
+cout << "/////////////////////////// " << endl;
 
-delete[] arr;
+std::ifstream inputfile("config.txt");
+int* arr_in = new int[N];
+
+if(inputfile.is_open())
+{
+    cout << "File open for read to array! " << endl;
+for(int i=0;i<N;i++)
+{
+    inputfile >> arr_in[i];
+}
+}
+else
+{
+cout << "Unable to read file! " << endl;
+}
+inputfile.close();
+
+for(int j=0;j<N;j++)
+{
+    cout << arr_in[j]<< endl;
+}
+delete[] arr_in;
+
     return 0;
 }
