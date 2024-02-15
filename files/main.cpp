941146e38549ea26ff54addc6ebc36b5a914d134
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-02-15 21:50:16 +0200

    files

--- files/main.cpp
+++ files/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <random>
 
 using std::cout;
 using std::endl;
@@ -6,6 +8,46 @@ using std::cin;
 
 int main()
 {
+    srand(time(NULL));
+const int N = 50;
+int* arr = new int[N];
 
+std::ifstream file;
+file.open("file.txt");
+if(file.is_open())
+{
+    cout << "File is open!" << endl;
+    for(int i=0;i<N;i++)
+    {
+
+       file >> arr[i];
+        cout << arr[i] << endl;
+    }
+}
+else
+{
+std::cerr << "File not found" << endl; 
+}
+file.close();
+
+
+std::ofstream newfile;
+newfile.open("newfile.txt");
+if(newfile.is_open())
+{
+    cout << "File is open! " << endl;
+for(int i=0;i<N;i++)
+{
+    newfile << arr[i] << endl;
+}
+}
+else
+{
+std::cerr << "File corrupt!" << endl;
+}
+
+newfile.close();
+
+delete[] arr;
     return 0;
 }
