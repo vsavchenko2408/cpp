016bde554f6fb5580a9b3c27c85da63e6998b1e9
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-02-20 14:02:45 +0200

    OOP

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -3,10 +3,32 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+class MyClass
+{
+public:
+void set_data(int x, int y)//setter
+{
+a = x;
+b = y;
+}
+
+void get_data()//getter
+{
+cout << "A = " << a << endl;
+cout << "B = " << b << endl;
+}
+
+private:
+int a;
+int b;
+};
+
 int main()
 {
+MyClass mc;//obj mc
 
-
+mc.set_data(4,6);
+mc.get_data();
 
 
     return 0;
