bb18a6853dfda84df8379fe844a81bb22c5268ba
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-02-23 10:22:19 +0200

    Constructor

--- Constructor&Destructor/main.cpp
+++ Constructor&Destructor/main.cpp
@@ -3,10 +3,30 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+class MyClass
+{
+public:
+MyClass(int a)
+{
+    x = a;
+    cout << "Constructor! " << endl;
+}
+int getX()
+{
+    return x;
+}
+~MyClass()
+{
+    cout << "Destructor" << endl;
+}
+private:
+int x;
+};
 int main()
 {
+MyClass mc(15);
 
-
+cout <<  mc.getX() << endl;
 
     return 0;
 }
