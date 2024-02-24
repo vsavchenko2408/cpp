d7342ef92e020d9c9b06244df2a59de1febbfd68
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-02-25 00:56:18 +0200

    Inheritance

--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -2,10 +2,43 @@
 using std::cout;
 using std::endl;
 using std::string;
+class Animals
+{
+public:
+void SetVoice(string voice)
+{
+    this->voice = voice;
+}
+string Say()
+{
+    return voice;
+}
+private:
+string voice;
+};
 
+
+class Cat : public Animals
+{
+public:
+bool vibrice;
+
+};
+
+
+
+class Dog : public Animals
+{
+    public:
+string tail;
+};
 
 int main()
 {
+    Cat cat;
+    cat.SetVoice("meow");
+    cat.vibrice = 20;
+    
 
     return 0;
 }
