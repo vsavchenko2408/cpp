5fd6afa85c95c0efb72d7494ff9cfb842802a64a
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-02-24 13:48:02 +0200

    thisother

--- thisother/main.cpp
+++ thisother/main.cpp
@@ -2,10 +2,25 @@
 using std::cout;
 using std::endl;
 
+class MyClass
+{
+public:
+void setx(int x)
+{
+ this->x = x;
+ cout << this << endl;   
+}
+private:
+int x;
+
+}; 
 
 int main()
 {
-
+MyClass a;
+a.setx(10);
+MyClass b;
+cout << &a << endl;
 
     return 0;
 }
