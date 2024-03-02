7f4cf17cf1854030fe97d666747bfd4da56ba0c5
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-03-02 16:36:17 +0200

    Operators

--- Operators/main.cpp
+++ Operators/main.cpp
@@ -2,10 +2,55 @@
 using std::cout;
 using std::endl;
 
+class Oper //класс оператор
+{
+public:
+            //прототипы методов
+Oper(){}//Конструктор по умолчанию
+Oper(int _x, int _y);//конструктор с параметрами
+void operator+(Oper& obj);//перегрузка оператора сложения
+Oper operator=(Oper& obj);//перегрузка оператора присваивания
+void show();//метод вывода
+
+private:
+int x;
+int y;
+};
 
 int main()
 {
 
+Oper a(5,4);
+Oper b(1,3);
+Oper c;
+c = a; // присваивание объекта "а" объекту "в"
+c.show(); // вывод результата
 
     return 0;
+}
+
+//тела методов
+Oper::Oper(int _x, int _y)
+{
+    x = _x;
+    y = _y;
+}
+
+void Oper::operator+(Oper& obj)
+{
+this->x + obj.x;
+this->y + obj.y;
+}
+
+void Oper::show()
+{
+    cout << "x: " << x << endl;
+    cout << "y: " << y << endl;
+}
+
+Oper Oper::operator=(Oper& obj)
+{
+this->x = obj.x;
+this->y = obj.y;
+return obj;
 }
