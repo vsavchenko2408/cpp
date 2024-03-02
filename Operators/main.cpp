31e495d9a21ac802af74d62ed45df24129d31e85
DerMeister <1985.der.meister.1985@gmail.com>
2024-03-02 14:16:14 +0200

    Operators

--- Operators/main.cpp
+++ Operators/main.cpp
@@ -5,21 +5,11 @@ using std::endl;
 class Oper
 {
 public:
-Oper(int _x, int _y)
-{
-   x= _x;
-   y= _y;
-}
-void show()
-{
-    cout << "x = " << this-> x << endl;
-    cout << "y = " << this-> y << endl;
-}
-void operator+(Oper& obj)
-{
-    x = this->x + obj.x;
-    y = this->y + obj.y;
-}
+Oper(int _x, int _y);
+void show();
+void operator+(Oper& obj);
+Oper operator=(Oper& obj);
+
 private:
 int x;
 int y;
@@ -28,8 +18,34 @@ int main()
 {
 Oper a(5,4);
 Oper b(3,1);
-a+b;
-
+Oper c = a;
+c.show();
 
     return 0;
+}
+
+
+Oper::Oper(int _x, int _y)
+{
+   x= _x;
+   y= _y;
+}
+
+void Oper::show()
+{
+    cout << "x = " << this-> x << endl;
+    cout << "y = " << this-> y << endl;
+}
+
+void Oper::operator+(Oper& obj)
+{
+    x = this->x + obj.x;
+    y = this->y + obj.y;
+}
+
+Oper Oper::operator=(Oper& obj)
+{
+this->x = obj.x;
+this->y = obj.y;
+return *this;
 }
