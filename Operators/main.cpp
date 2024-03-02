3f374de2e16ee5d7ac0d4cc40df923dea3f708e8
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-03-02 15:13:54 +0200

    Edit Operators

--- Operators/main.cpp
+++ Operators/main.cpp
@@ -2,50 +2,9 @@
 using std::cout;
 using std::endl;
 
-class Oper
-{
-public:
-Oper(int _x, int _y);
-void show();
-void operator+(Oper& obj);
-Oper operator=(Oper& obj);
 
-private:
-int x;
-int y;
-};
 int main()
 {
-Oper a(5,4);
-Oper b(3,1);
-Oper c = a;
-c.show();
 
     return 0;
-}
-
-
-Oper::Oper(int _x, int _y)
-{
-   x= _x;
-   y= _y;
-}
-
-void Oper::show()
-{
-    cout << "x = " << this-> x << endl;
-    cout << "y = " << this-> y << endl;
-}
-
-void Oper::operator+(Oper& obj)
-{
-    x = this->x + obj.x;
-    y = this->y + obj.y;
-}
-
-Oper Oper::operator=(Oper& obj)
-{
-this->x = obj.x;
-this->y = obj.y;
-return *this;
 }
