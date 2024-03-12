598d8ef6b01f7669e8b73c10dbd6f9311a5752b7
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-03-12 14:09:48 +0200

    Friend

--- Friend/main.cpp
+++ Friend/main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 using std::cout;
 using std::endl;
+class Frnd;
+class MyClass
+{
+public:
+friend class Frnd;
 
+private:
+    int x = 10;
+};
+
+class Frnd
+{
+
+public:
+
+void show(MyClass &obj)
+{
+    obj.x = 15;
+    cout << obj.x << endl;
+}
+};
 
 
 int main()
 {
+MyClass mc;
 
+Frnd fd;
+fd.show(mc);
 
     return 0;
 }
