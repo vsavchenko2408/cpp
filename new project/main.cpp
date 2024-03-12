5385526de67715235ff32ca717169b25cdd51fc8
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-03-12 12:00:32 +0200

    new project

--- new project/main.cpp
+++ new project/main.cpp
@@ -2,9 +2,23 @@
 using std::cout;
 using std::endl;
 
+
+
 int main()
 {
 
+    int a = 9;
+    int b = 10;
+    int &aref = a;
+    int &bref = b;
+if(aref<bref)
+{
+    cout << "True" << endl;
+}
+else cout << "False" << endl;
+
+
+//cout <<  << endl;
 
 
     return 0;
