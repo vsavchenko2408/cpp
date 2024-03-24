47f378d9b3ec6214671918de0e9ed14265d45282
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-03-24 12:58:43 +0200

    sync

--- vectors/main.cpp
+++ vectors/main.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
+#include <vector>
+#include <string>
+using std::vector;
 using std::cout;
 using std::cin;
 using std::endl;
+
 int main()
 {
+
+
+vector<float> MyVector(15);
+ MyVector.reserve(100); 
+for(int i=0;i<MyVector.size();i++)
+{
+MyVector[i] = i;
+}
+for(int j=0;j<MyVector.size();j++)
+{
+    cout << MyVector[j] << endl;
+} 
+
+
+cout << MyVector[4] << endl;
 
  return 0;
 }
