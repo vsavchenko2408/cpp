a3cdc76d9b317a6f2255001c8beff356913aff17
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-03-25 12:51:50 +0200

    Iterators

--- Iterators/main.cpp
+++ Iterators/main.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
-#include <array>
-using std::array;
 using std::cout;
 using std::endl;
 
 int main()
 {
-array<int, 15> a;
 
-a.fill(10);
-for(auto x: a)
-{
-    cout << x;
-}
     return 0;
 }
