c968fabf08ccda86a9b4892091cad6845005c76f
DerMeister1985 <1985.der.meister.1985@gmail.com>
2024-03-26 20:22:47 +0200

    sync

--- Iterators/main.cpp
+++ Iterators/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <vector>
 using std::cout;
 using std::endl;
+using std::vector;
 
 int main()
 {
+
+vector<int> V = {45,65,34,76,98,23,54,45,67,90};
+vector<int>::reverse_iterator it;
+
+for(it=V.rbegin();it!=V.rend();it++ )
+{
+    cout << *it << endl;
+   
+}
 
     return 0;
 }
