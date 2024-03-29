a4c16ab5ddaf3b72b535db21ae4c61199233e24c
DerMeister <1985.der.meister.1985@gmail.com>
2024-03-29 19:09:27 +0200

    list

--- list/main.cpp
+++ list/main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
+#include <list>
 using std::endl;
 using std::cout;
+using std::list;
 
-int main()
+void prntlst(const list<int> &lst)
 {
 
+list<int>::const_iterator i ;
+for(i= lst.begin();i != lst.end(); i++)
+{
+cout << *i << endl;
 
+}
+
+
+}
+int main()
+{
+list<int> lst = {54,32,76,45,65,12,54,54,43,76,72,83};
+list<int>::iterator it = lst.begin();
+lst.push_back(13);
+lst.push_front(1);
+lst.insert(it , 5);
+lst.pop_front();
+lst.pop_back();
+//lst.erase(it);
+//lst.sort();
+lst.unique();
+prntlst(lst);
+/*
+cout << *it << endl;
+advance(it, 4);
+advance(it,1);
+cout << *it << endl;
+*/
     return 0;
 }
