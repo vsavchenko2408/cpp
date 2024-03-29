fe7c186c71bf9890416f2ba5aa72a45f3f632f5f
DerMeister <1985.der.meister.1985@gmail.com>
2024-03-29 22:08:46 +0200

    HomeworkLesson2

--- HillelLesson2HomeWork/main.cpp
+++ HillelLesson2HomeWork/main.cpp
@@ -1,10 +1,53 @@
 #include <iostream>
 using std::cout;
 using std::endl;
+static unsigned int cntptr = 0;//лічильник
+
+template<class T>
+class SmartPointer
+{
+public:
+
+
+SmartPointer(T* ptr)//конструктор копіювання
+{
+    cntptr++;
+this->ptr = ptr;
+}
+
+SmartPointer()//конструктор за замовчуванням
+{
+    cntptr++;
+
+}
+SmartPointer operator=(T &obj)//перегрузка оператора присвоєння
+{
+    cntptr++;
+    *this->ptr = obj;
+    return *this;
+}
+
+SmartPointer operator*(T &obj)//перегрузка оператора розіменування
+{
+    return *this;
+}
+
+~SmartPointer() //деструктор
+{
+    if(cntptr == 0) delete this->ptr;
+    else cntptr--;
+}
+
+private:
+T* ptr = new T;
+};
+
 
 int main()
 {
-
+int a = 50;
+SmartPointer<int> sm ;
+sm = a;
 
 
     return 0;
