4374cc198abd9f3adf9763432c8cb31da30b6cc3
DerMeister <1985.der.meister.1985@gmail.com>
2024-04-02 14:37:05 +0300

    deque

--- deque/main.cpp
+++ deque/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <deque>
 
 using std::cout;
 using std::endl;
 
 int main()
 {
+
+std::deque<int> dq;
+
+dq.push_front(5);
+dq.push_back(14);
+dq.push_back(32);
+
+std::deque<int>::iterator it;
+it = dq.begin();
+
+for(it;it != dq.end();it++)
+{
+    cout << *it << endl;
+}
+
+
 
 
 
