bd74be758b1213bb3b015bddf6d8bd52cd4a1ba8
DerMeister <1985.der.meister.1985@gmail.com>
2024-04-04 11:07:03 +0300

    HomeWork_3.4

---
+++ HomeWork_3.4/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <random>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int arr[100];
+for(int i=0;i<100;i++)
+{
+    arr[i] = std::random_device{}();
+}
+for (auto x: arr)
+{
+    cout << x << endl;
+}
+system("pause");
+    return 0;
+}
