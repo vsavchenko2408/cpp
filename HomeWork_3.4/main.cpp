357a4a3c9c2e486ddf021a80939b11494093b557
DerMeister <1985.der.meister.1985@gmail.com>
2024-04-04 19:06:30 +0300

    HomeWork_3.4

--- HomeWork_3.4/main.cpp
+++ HomeWork_3.4/main.cpp
@@ -1,20 +1,63 @@
+//Мені допамагав chatGPT. Я чесно зізнаюсь що мало розімую як мій код працює
+
+
 #include <iostream>
 #include <random>
+#include <unordered_map>
 using std::cout;
 using std::endl;
+using std::string;
+/*
+std::random_device rd; // виніс у глобальну зону видимості бо інакше в мене видає одне й те саме значення
+std::uniform_int_distribution<int> dist(1, 100);
+int myrand() // зробив зручну функцію яка видає рандомне число але не бачу де вона використовується і код працює без неї
+{
+    return dist(rd);
+}
+*/
+
+class MyClass // Мій класс
+{
+public:
+    MyClass() = default; //я не знав що так можна створювати конструктор за замовчуванням
+
+    MyClass(string idValue)
+    {
+        id = idValue;
+    }
+
+    size_t hash() const // я досі не розумію що за тип даних size_t
+    {
+        return std::hash<string>{}(id); // chatGPT сказав можна користуватись функцією яка є в STL.
+    }
+
+    string getId() const // метод що повертає сирий ID
+    {
+        return id;
+    }
+
+private:
+    string id;
+};
 
 int main()
 {
-    std::random_device rd;
-    int arr[100];
-for(int i=0;i<100;i++)
-{
-    arr[i] = std::random_device{}();
-}
-for (auto x: arr)
-{
-    cout << x << endl;
-}
-system("pause");
+
+    std::unordered_map<size_t, MyClass> uomap;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        std::string id = "ID: " + std::to_string(i);
+        MyClass obj(id);
+        uomap[obj.hash()] = obj;
+    }
+
+    
+    for ( auto x : uomap)
+    {
+        std::cout << "Key: " << x.first << ", Value: " << x.second.getId() << std::endl;
+    }
+
+    system("pause");
     return 0;
 }
