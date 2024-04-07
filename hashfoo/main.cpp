bff1273d1ff5e7c36bd7f24e2db2d18f83307a00
DerMeister <1985.der.meister.1985@gmail.com>
2024-04-08 00:23:14 +0300

    hashfoo

---
+++ hashfoo/main.cpp
@@ -0,0 +1,30 @@
+#define ui unsigned int 
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int hash(const string& str)
+{
+ui id;
+for(auto x: str)
+{
+    id += x;
+}
+
+return id;
+}
+
+int main()
+{
+cout << "Enter some text: " ;
+string str;
+
+cin >> str;
+cout << "Hash code: " << hash(str) << endl;
+system("pause");
+    return 0;
+}
