9f3aa01db1ccda8aae11acfefc7bcd6b9fa8c68d
DerMeister <1985.der.meister.1985@gmail.com>
2024-04-10 05:32:35 +0300

    unorderedmap

--- unorderedmap/main.cpp
+++ unorderedmap/main.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 #include <unordered_map>
+#include <map>
 #include <random>
 using std::cout;
 using std::endl;
 using std::unordered_map;
+using std::map;
 
 int main()
 {
     srand(time(NULL));
     
 unordered_map< int, int> uomap;
-for(auto i: uomap)
+for(int i =0; i<10;++i)
 {
 uomap.emplace(rand()%10, rand()%100);
 }
 
+for(auto j: uomap)
+{
+    cout << j.first << ", " << j.second << endl;
+}
 
+map<int,int> newmap(uomap.begin(), uomap.end());
+uomap.clear();
+cout << "Converted" << endl;
+for(auto j: newmap)
+{
+    cout << j.first << ", " << j.second << endl;
+}
 
+return 0;
 }
