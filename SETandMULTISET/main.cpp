fa14f0e444e2d9d5d9d2f1d322778ff8b049be9f
DerMeister <1985.der.meister.1985@gmail.com>
2024-04-16 16:55:27 +0300

    sync

--- SETandMULTISET/main.cpp
+++ SETandMULTISET/main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
+#include <set>
 using std::cout;
 using std::endl;
+
 
 int main()
 {
 
+std::set<int> st = {14,6,3,45,23};
 
+st.insert(15);
+st.insert(15);
+st.erase(6);
+st.erase(99);
 
+std::set<int>::iterator it = st.begin();
+
+for(it;it != st.end();it++)
+{
+    cout << *it << endl;
+}
+
+int fnd;
+std::cin >> fnd;
+if(st.find(fnd) != st.end())
+{
+    cout << "найдено! " << fnd << endl;
+}
+else
+{
+    cout << "Не найдено! " << fnd << endl;
+}
 
     return 0;
 }
