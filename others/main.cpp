a7fc06211b5f9c161d5b100bff92e76a3c5c6c07
DerMeister <1985.der.meister.1985@gmail.com>
2024-04-18 06:51:57 +0300

    sync

--- others/main.cpp
+++ others/main.cpp
@@ -1,33 +1,22 @@
 #include <iostream>
-#include <memory>
-#include <string>
+
 using std::cout;
 using std::endl;
 
-
-template <class T>
-T foo(T obj)
+class MyString
 {
-auto x = std::make_shared<T> (obj);
-cout << x.use_count() << endl;
-    return *x;
-}
+public:
+MyString(){}
+
+
+
+
+private:
+char* str = new char[50];
+};
 
 int main() {
 
-int* a = new int(10);
 
-auto t = std::make_shared<int>(foo(*a));
-
-auto x = std::make_shared<int>(*t);
-cout << "Count t: " << t.use_count() << endl;
-cout << "Count x: " << x.use_count() << endl;
-
-
-
-cout << "T: " << *t << endl;
-cout << "X: " << *x << endl;
-
-system("pause");
     return 0;
 }
