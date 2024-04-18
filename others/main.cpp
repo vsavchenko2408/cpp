9fb8287535d1a786ec6adc0a77df70913536eef1
DerMeister <1985.der.meister.1985@gmail.com>
2024-04-18 07:09:19 +0300

    others

--- others/main.cpp
+++ others/main.cpp
@@ -1,22 +1,44 @@
 #include <iostream>
-
+using std::cin;
 using std::cout;
 using std::endl;
 
 class MyString
 {
 public:
-MyString(){}
+MyString(){str = nullptr;}
+MyString(char _str[])
+{
+str = _str;
+}
 
+void print_str()
+{
+    cout << str << endl;
+}
+void set_string()
+{
+    cin >> str[50];
+}
+void operator=(char _str[])
+{
+    str = _str;
+}
 
-
-
+~MyString()
+{
+    delete [] str;
+}
 private:
 char* str = new char[50];
 };
 
 int main() {
 
+cout << "Input some text: " << endl;
 
+MyString a;
+a.set_string();
+a.print_str();
     return 0;
 }
