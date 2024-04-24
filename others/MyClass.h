e5201f007e9235375c14172f89fc31b041e9b323
DerMeister <1985.der.meister.1985@gmail.com>
2024-04-24 09:39:36 +0300

    sync

---
+++ others/MyClass.h
@@ -0,0 +1,15 @@
+#ifndef ADD_H
+#define ADD_H
+
+class MyClass
+{
+public:
+MyClass();
+MyClass(int,int,int);
+void set_xyz(int _x, int _y, int _z);
+void show_xyz();
+private:
+int x,y,z;
+};
+
+#endif
--- others/main.cpp
+++ others/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <map>
+#include "MyClass.h"
+//#include <map>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -8,45 +9,10 @@ using std::endl;
 
 int main() 
 {
-std::pair<int ,std::string> p1,p2,p3;
-p1.first = 1;
-p1.second = "sometext1";
-p2.first = 2;
-p2.second = "sometext2";
-p3.first = 3;
-p3.second = "sometext3";
+MyClass mc;
+mc.set_xyz(4,2,6);
+mc.show_xyz();
 
-
-
-std::map <int , std::string> m;
-std::map <std::string, u_int> m2;
-
-m.insert(p1);
-m.insert(p2);
-m.insert(p3);
-m.insert(std::make_pair(4, "sometext4"));
-m.insert(std::make_pair(4, "sometext4"));
-m.insert(std::pair<int,std::string>(5,"sometext5"));
-m.emplace(6,"sometext6");
-
-
-for(auto i: m)
-{
-    m2.emplace(i.second , i.first);
-}
-
-for(auto x: m)
-{
-    
-    cout << x.first << " " << x.second << endl;
-}
-
-cout <<endl << endl;
-for(auto x: m2)
-{
-    
-    cout << x.first << " " << x.second << endl;
-}
-
+system("pause");
     return 0;
 }
