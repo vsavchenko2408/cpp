d12739d9b089001bb73fcc5c64a0867eab4570ea
DerMeister <1985.der.meister.1985@gmail.com>
2024-04-24 23:35:12 +0300

    SFML

---
+++ SFMLtest/main.cpp
@@ -0,0 +1,18 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+sf::RenderWindow window(sf::VideoMode(800,600), "SFML Window");
+while(window.isOpen())
+{
+sf::Event event;
+while(window.pollEvent(event))
+{
+    if (event.type == sf::Event::Closed)
+    window.close();
+}
+window.clear(sf::Color::Red);
+window.display();
+}
+return 0;
+}
