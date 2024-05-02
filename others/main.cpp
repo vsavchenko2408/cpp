ac1abd746bb466457c32cfa114b725d25e5df181
DerMeister <1985.der.meister.1985@gmail.com>
2024-05-02 16:10:33 +0300

    others

--- others/main.cpp
+++ others/main.cpp
@@ -1,18 +1,46 @@
 #include <iostream>
-#include "MyLibrary.h"
 #include <map>
 using std::cin;
 using std::cout;
 using std::endl;
-using MyLib::MyClass;
-
+template<class T>
+class A
+{
+    public:
+    A(){}
+    A(T _data)
+    {
+        if(_data > 100)
+        { throw _data;}
+        else
+        data = _data;
+    }
+    void get_data()
+    {
+        return data;
+    }
+    void set_data(T _data)
+    {
+         if(_data > 100)
+        { throw _data;}
+        else
+        data = _data;
+    }
+private:
+T data;
+};
 
 int main() 
 {
-MyLib::MyClass mc;
 
-mc.set_data(15);
-cout << mc.get_data();
+A<int> a(15);
+try{
+a.set_data(200.56);
+}
+catch(const T& x)
+{
+    cout << "Exception: " << x << endl;
+}
 
 
 //system("pause");
