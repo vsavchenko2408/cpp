450ff157d17a29f8e57415e26053345fc3428014
DerMeister <1985.der.meister.1985@gmail.com>
2024-05-06 00:42:00 +0300

    others

--- others/main.cpp
+++ others/main.cpp
@@ -1,48 +1,28 @@
 #include <iostream>
-#include <map>
+#include <memory>
 using std::cin;
 using std::cout;
 using std::endl;
-template<class T>
-class A
+class MyClass
 {
     public:
-    A(){}
-    A(T _data)
-    {
-        if(_data > 100)
-        { throw _data;}
-        else
-        data = _data;
-    }
-    void get_data()
-    {
-        return data;
-    }
-    void set_data(T _data)
-    {
-         if(_data > 100)
-        { throw _data;}
-        else
-        data = _data;
-    }
-private:
-T data;
+    MyClass() = default;
+    MyClass(int _data){ data = _data;}
+    int data;
 };
+
+MyClass foo(MyClass a)
+{
+std::shared_ptr<MyClass> ptr(a);
+    
+    //*ptr+=5;
+    return *ptr;
+}
 
 int main() 
 {
+    MyClass* a = new MyClass(15);
+    MyClass newptr = foo(*a);
 
-A<int> a(15);
-try{
-a.set_data(200.56);
-}
-catch(const T& x)
-{
-    cout << "Exception: " << x << endl;
-}
-
-
-//system("pause");
     return 0;
 }
