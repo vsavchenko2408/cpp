4f4e3c77d3ca883faeccbd4e37e1fe9da8430fe2
DerMeister <1985.der.meister.1985@gmail.com>
2024-05-09 20:39:00 +0300

    lambdafunc

--- lambdafunc/main.cpp
+++ lambdafunc/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using std::cout;
 using std::endl;
+using std::vector;
+
 
 int main()
 {
+int a = 15;
 
-
-
+auto f = [&a](){return a*10;};
+cout << f() << endl;
+cout << "A: " << a << endl;
     return 0;
 }
