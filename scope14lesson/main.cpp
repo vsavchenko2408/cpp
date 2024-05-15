c300695620106746f8eec0c85cf850fe5a17d0d7
DerMeister <1985.der.meister.1985@gmail.com>
2024-05-15 21:43:33 +0300

    scope14lesson

--- scope14lesson/main.cpp
+++ scope14lesson/main.cpp
@@ -8,6 +8,7 @@ void foo1();
 
 int main()
 {  
+    setlocale(LC_ALL,"ru");
     int x = 20;
     int y = 45;
 foo2();
@@ -24,4 +25,5 @@ void foo2()
 {
     foo1();
     cout << "Вызвалась функция foo2()" << endl;
+    system("pause");
 }
