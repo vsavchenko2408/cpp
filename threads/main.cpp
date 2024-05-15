142aa0edb2f35fa7dd2c179229b33b06619a1b27
DerMeister <1985.der.meister.1985@gmail.com>
2024-05-15 23:48:01 +0300

    sync

--- threads/main.cpp
+++ threads/main.cpp
@@ -1,40 +1,46 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
 #include <chrono>
 using namespace std;
+//RAII
+
+mutex m;
 
 void foo1()
 {
-for(int i = 0;i<10;++i)
+    lock_guard<mutex> guard(m);
+cout << "ID " << this_thread::get_id() << endl;
+for(int i=0; i< 5; i++)
 {
-    std::this_thread::sleep_for(chrono::milliseconds(300));
-    cout << this_thread::get_id() << " " << "Foo1: " << i << endl;
+    this_thread::sleep_for(chrono::milliseconds(500));
+   cout << "Foo1!" << endl;  
 }
+
 }
+/*
 void foo2()
 {
-for(int i = 0;i<10;++i)
+cout << "ID " << this_thread::get_id() << endl;
+for(int i=0; i< 10; i++)
 {
-    std::this_thread::sleep_for(chrono::milliseconds(500));
-    cout << this_thread::get_id() << " " << "Foo2: " << i << endl;
+    this_thread::sleep_for(chrono::milliseconds(300));
+   cout << "Foo2!" << endl;  
 }
 }
-void foo3()
-{
-for(int i = 0;i<10;++i)
-{
-    std::this_thread::sleep_for(chrono::milliseconds(700));
-    cout << this_thread::get_id() << " " << "Foo2: " << i << endl;
-}
-}
+*/
 int main()
 {
 
 std::thread th1(foo1);
-std::thread th2(foo2);
-std::thread th3(foo3);
+std::thread th2(foo1);
+
+
+
+
+
+
 th1.join();
 th2.join();
-th3.join();
     return 0;
 }
