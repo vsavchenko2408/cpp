707792784448b321d79091a40dc86e93e6e751f9
DerMeister <1985.der.meister.1985@gmail.com>
2024-05-24 01:21:27 +0300

    Allocator

--- Allocator/main.cpp
+++ Allocator/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
-template<class T, class Allocator = std::allocator<T>> class vector;
+#include <memory>
+
+
 
 
 int main()
 {
+std::allocator<float> allfl;
+float* arr = allfl.allocate(5);
 
+for(int i=0;i<5;++i)
+{
+allfl.construct(arr+i,i+1);
+}
 
-
+for(int j=0;j<5;++j)
+{
+    std::cout << arr[j] << std::endl;
+}
+allfl.deallocate(arr, 5);
+system("pause");
 }
