15dd8916d6692a5a2143aabacb75b348951429df
DerMeister <v.savchenko2408@gmail.com>
2024-05-30 03:47:03 +0300

    Singletone

--- Singletone/main.cpp
+++ Singletone/main.cpp
@@ -6,11 +6,24 @@ using std::endl;
 class A
 {
 A(){}
+A(A &obj){}
 
-
+int data;
+public:
+A(int _data)
+{
+    data = _data;
+}
+int get_data()
+{
+    return data;
+}
 };
+
 int main()
 {
+
+A obj(10);
 
 
 
