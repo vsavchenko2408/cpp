de394b3b28c213f1b2bc46a9f51f7e5b315df1d8
DerMeister <v.savchenko2408@gmail.com>
2024-05-31 08:51:33 +0300

    ptrfoo

---
+++ ptrfoo/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int foo(int data)
+{
+    data +=100;
+    return data;
+}
+
+int main()
+{
+
+int(*ptrfoo) (int)= foo;
+cout << (*ptrfoo)(15) << endl;
+
+
+}
