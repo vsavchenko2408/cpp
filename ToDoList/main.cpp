8253bbd330b25f44a4cd11469eaaa8cb8593ac63
Der Meister <v.savchenko2408@gmail.com>
2024-06-04 23:26:00 +0300

    ToDoList

--- ToDoList/main.cpp
+++ ToDoList/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "task.h"
 #include <vector>
-
+#include <chrono>
+#include <thread>
+#include <cstdlib>
 
 
 int main()
 {
+    /*
     std::vector<Task> Tasks;
     Task job;
     Task home("To Home", false, 2);
@@ -15,5 +18,15 @@ int main()
     {
         x.task_info();
     }
+    */
+  auto data = std::chrono::milliseconds(10);
+  for(auto i = data.count(); i!=0;--i)
+  {
+std::cout << i << std::endl;
+std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
+  system("beep -f 440 -l 500");
+    //localtime();
+
     return 0;
 }
--- ToDoList/task.h
+++ ToDoList/task.h
@@ -6,13 +6,14 @@ class Task
     std::string work;
     bool status;
     int priority;
+    
 
 public:
     Task( std::string _work, bool _status, int _priority);  //args constr
-    Task();                                       //default const
+    Task();                                                 //default const
     void task_info();                                       //display all info
-    void set_priority(int _priority);
-    void complete();
+    void set_priority(int _priority);                       //set priority
+    void complete();                                        //change status
 
 };
 
