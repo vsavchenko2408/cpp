89f7b0bd912a620b78987b7251bf4363c1214798
DerMeister <v.savchenko2408@gmail.com>
2024-06-25 08:47:57 +0300

    add testclass

---
+++ testclass/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class Pointer
+{
+int x;
+int y;
+public:
+Pointer(int _x, int _y)
+{
+    x = _x;
+    y = _y;
+}
+Pointer(){x=0; y=0;}
+
+
+
+};
+
+int main()
+{
+
+}
