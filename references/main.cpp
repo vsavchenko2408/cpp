86e5eaf6d99762d57bcab8c98d0ecbf5e4e1d1a3
DerMeister <v.savchenko2408@gmail.com>
2024-06-26 10:20:48 +0300

    Create references

---
+++ references/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class MC
+{
+    int data;
+public:
+    MC(int data)
+    {
+        this->data = data;
+    }
+    MC(){data = 0;}
+    void set_data(int data)
+    {
+        this->data = data;
+    }
+    void show_data()
+    {
+        std::cout << "Data: " << data << std::endl;
+    }
+    int get_data()
+    {
+        return data;
+    }
+};
+
+void foo(MC &obj, int value)
+{
+    obj.set_data(value);
+}
+int main()
+{
+    MC a;
+    foo(a, 15);
+    a.show_data();
+
+
+
+    return 0;
+}
