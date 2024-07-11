ec126e58ffbc6853b9f64c040bc47c103ac40c78
DerMeister <v.savchenko2408@gmail.com>
2024-07-11 23:59:13 +0300

    SFMLtest

--- SFMLtest/main.cpp
+++ SFMLtest/main.cpp
@@ -1,5 +1,5 @@
 #include <SFML/Graphics.hpp>
-
+#include <chrono>
 using namespace sf;
 int main()
 {
@@ -12,8 +12,19 @@ while(window.pollEvent(event))
     if (event.type == sf::Event::Closed)
     window.close();
 }
-window.clear(sf::Color::Red);
+window.clear(sf::Color(250,220,100,0));
+
+sf::CircleShape circle(50.f);
+
+circle.setFillColor(Color(230, 0, 230));
+for(int i=0;i<100;i++)
+{
+circle.move(i+10,i+10);
+
+window.draw(circle);
+
 window.display();
+}
 }
 return 0;
 }
