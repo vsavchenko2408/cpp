5fb04bbe945fe06e17c54fb88285c551a4ce2a92
DerMeister <v.savchenko2408@gmail.com>
2024-07-18 01:45:58 +0300

    arrays

--- arrays/main.cpp
+++ arrays/main.cpp
@@ -2,15 +2,55 @@
 #include <random>
 using std::cout;
 using std::endl;
+template<class T>
+void swap(T& a, T& b);
+
 
 template<class T>
-void fillarr(T& arr, int N)
+void fillarr(T& arr, int N); //fill array random value
+
+template <class T>
+void showarr(T& arr, int N); //Display array
+
+template <class T>
+void sort(T& arr, int N)
 {
-    for(int i=0;i<N;i++)
+for(int i=0;i<N;i++)
+{
+    for(int j=0;j < N;j++)
     {
-        arr[i] = rand()%999;
+        if(arr[i] < arr[j])
+        {
+          swap(arr[i], arr[j]);  
+        }
     }
 }
+
+
+}
+/////////////////////////////////////////////////////////////////////
+int main()
+{
+
+const int N = 50;
+float arr[N];
+fillarr(arr, N);
+showarr(arr, N);
+sort(arr, N);
+showarr(arr, N);
+
+system("pause");
+    return 0;
+}
+//////////////////////////////////////////////////////
+template <class T>
+void swap(T& a, T& b)
+{
+    T temp = a;
+    a = b;
+    b = temp; 
+}
+
 template <class T>
 void showarr(T& arr, int N)
 {
@@ -21,15 +61,12 @@ for(auto i: arr)
 cout << endl;
 }
 
-int main()
+template<class T>
+void fillarr(T& arr, int N)
 {
-srand(time_t(NULL));
-const int N =5;
-int arr[N];
-fillarr(arr, N);
-showarr(arr, N);
-
-
-system("pause");
-    return 0;
+    srand(time_t(NULL));
+    for(int i=0;i<N;i++)
+    {
+        arr[i] = rand()%999;
+    }
 }
