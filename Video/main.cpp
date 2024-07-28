dba735a482e07e450a0312258ac94e90593c5bb1
DerMeister <v.savchenko2408@google.com>
2024-07-28 22:25:13 +0300

    Video

--- Video/main.cpp
+++ Video/main.cpp
@@ -3,7 +3,21 @@
 
 int main()
 {
+const int SIZE = 50;
+int arr[SIZE];
+int sum = 0;
+for(int i=0;i<SIZE;++i)
+{
+    arr[i] = i+1;
+    std::cout << "Element: " << i << " = " << arr[i] << std::endl; 
+}
 
+for(int j=0;j<SIZE;++j)
+{
+    sum += arr[j];
+}
+
+std::cout << "Sum of all elements: " << sum << std::endl;
 
     return 0;
 }
