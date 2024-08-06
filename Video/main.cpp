2efa215897484d2ab71034cce960047c468e7cb1
Dermeister <v.savchenko2408@gmail.com>
2024-08-06 10:36:15 +0300

    Edit Video

--- Video/main.cpp
+++ Video/main.cpp
@@ -3,25 +3,8 @@
 
 int main()
 {
-    const int ROW = 10;
-    const int COL = 10;
-int arr[COL][ROW];
 
-for(int i =0; i < COL;++i)
-{
-    for(int j =0; j<ROW; ++j)
-    {
-       arr[i][j] = i*j;
-    }
-}
-for(int i =1; i < COL;++i)
-{
-    for(int j =1; j<ROW; ++j)
-    {
-      std::cout << arr[i][j] << "  ";
-    }
-    std::cout << std::endl;
-}
+
 
 
     return 0;
