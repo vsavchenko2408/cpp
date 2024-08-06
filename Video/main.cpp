e3e259491bbe9e702c5e1ac8a337d9f21da986ac
DerMeister <v.savchenko2408@google.com>
2024-08-06 17:34:30 +0300

    Sync

--- Video/main.cpp
+++ Video/main.cpp
@@ -4,14 +4,6 @@
 int main()
 {
 
-std::cout << "char: \t" << sizeof(char) << std::endl;
-std::cout << "short: \t" << sizeof(short) << std::endl;
-std::cout << "int: \t" << sizeof(int) << std::endl;
-std::cout << "long: \t" << sizeof(long) << std::endl;
-std::cout << "long long: \t" << sizeof(long long) << std::endl;
-std::cout << "float: \t" << sizeof(float) << std::endl;
-std::cout << "double: \t" << sizeof(double) << std::endl;
-
 
 
 
