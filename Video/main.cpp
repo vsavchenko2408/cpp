5f72d454f84414b8c78db1b1ff317982a64f0e7d
DerMeister <v.savchenko2408@google.com>
2024-08-06 18:07:40 +0300

    Video 13

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
-
+using std::cout;
+using std::endl;
 
 int main()
 {
+int a;
 
+char c[4];
+cout << (int)c[6];
 
-
+cout << "char: \t\t" << sizeof(char) << " байт" << endl;
+cout << "short: \t\t" << sizeof(short) << " байт" << endl;
+cout << "integer: \t" << sizeof(int) << " байт" << endl;
+cout << "long: \t\t" << sizeof(long) << " байт" << endl;
+cout << "long long: \t" << sizeof(long long) << " байт" << endl;
+cout << "float: \t\t" << sizeof(float) << " байт" << endl;
+cout << "double: \t" << sizeof(double) << " байт" << endl;
 
     return 0;
 }
