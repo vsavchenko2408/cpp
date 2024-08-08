7a24e4ede07b96fd44e6d3afef6f7a5a9f5017c4
DerMeister <v.savchenko2408@google.com>
2024-08-08 11:06:51 +0300

    Video

--- Video/main.cpp
+++ Video/main.cpp
@@ -4,13 +4,6 @@ using std::endl;
 
 int main()
 {
-    setlocale(LC_ALL,"ua");
-    char c;
-    for(int i=0;i<127;++i)
-    {
-        cout << "Code: " << i << " " << c << endl;
-    c++;
-    }
 
     return 0;
 }
