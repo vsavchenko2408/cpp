73e05eec43bce935e2edae7a25836245af4126fb
DerMeister <v.savchenko2408@gmail.com>
2024-08-17 20:05:58 +0300

    sync

--- Video/main.cpp
+++ Video/main.cpp
@@ -3,6 +3,17 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+void foo(int count)
+{
+	cout << "Enter " << count << endl;
+	
+	if (count > 0)
+	{
+		foo(count - 1);
+		cout << "Exit " << count << endl;
+		return;
+	}
+}
 
 int main()
 {
