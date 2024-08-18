d823bc5ca5abc00e6b90129b1c7f5f5d5e6d159e
DerMeister <v.savchenko2408@google.com>
2024-08-18 17:30:58 +0300

    Video

--- Video/main.cpp
+++ Video/main.cpp
@@ -3,21 +3,7 @@ using std::cout;
 using std::endl;
 using std::cin;
 
-<<<<<<< HEAD
-void foo(int count)
-{
-	cout << "Enter " << count << endl;
-	
-	if (count > 0)
-	{
-		foo(count - 1);
-		cout << "Exit " << count << endl;
-		return;
-	}
-}
 
-=======
->>>>>>> 981905c50d37edd2d5d4808feaefd3fb4573fd1a
 int main()
 {
 
