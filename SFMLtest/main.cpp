f3a7b1dd5ed474ccf936b0aa2458b3dd98b7ab31
DerMeister <v.savchenko2408@google.com>
2024-08-21 03:43:49 +0300

    SFMLtest

--- SFMLtest/main.cpp
+++ SFMLtest/main.cpp
@@ -1,30 +1,17 @@
 #include <SFML/Graphics.hpp>
-#include <chrono>
+#include <SFML/Window.hpp>
+
 using namespace sf;
 int main()
 {
-sf::RenderWindow window(sf::VideoMode(800,600), "SFML Window");
-while(window.isOpen())
+sf::Window game(VideoMode(800,600), "Test window");
+while(game.isOpen())
 {
-sf::Event event;
-while(window.pollEvent(event))
-{
-    if (event.type == sf::Event::Closed)
-    window.close();
+    Event event;
+
+
+    game.display();
 }
-window.clear(sf::Color(250,220,100,0));
 
-sf::CircleShape circle(50.f);
-
-circle.setFillColor(Color(230, 0, 230));
-for(int i=0;i<100;i++)
-{
-circle.move(i+10,i+10);
-
-window.draw(circle);
-
-window.display();
-}
-}
 return 0;
 }
