8c8c0846f84aa66891bfa52283f270a93d721ea1
DerMeister <v.savchenko2408@google.com>
2024-08-22 21:22:05 +0300

    sync

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-#include <iostream>
-using std::cout;
-using std::endl;
-using std::cin;
-
-int main()
-{
-
-    return 0;
-}
-=======
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -22,4 +10,3 @@ int main()
     return 0;
 }
 
->>>>>>> 06282785ba82df6812dd8950e66e22e5b4f27ff6
