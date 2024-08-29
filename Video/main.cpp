1132a246c5040b6cf97a4465a4258b067408eda7
Viacheslav <v.savchenko2408@gmail.com>
2024-08-29 11:40:17 +0300

    Video

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
-
 using std::cout;
 using std::endl;
 using std::cin;
 
 int main()
 {
-    int a = 5;
-    int b = 10;
-
-    a<b ? cout << a << endl : cout << b << endl;
 
 system("pause");
     return 0;
