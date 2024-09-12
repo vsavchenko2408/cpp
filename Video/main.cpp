f27fce05f71adabe6d99e156a352f69963bd7c25
DerMeister <v.savchenko2408@google.com>
2024-09-12 16:46:22 +0300

    sync

--- Video/main.cpp
+++ Video/main.cpp
@@ -5,10 +5,53 @@ using std::endl;
 using std::cin;
 using std::string;
 
+class A
+{
+int SIZE;
+int* arr = new int[SIZE];
 
+public:
+A(){}
+A(int* _arr, int N)
+{
+     SIZE = N; 
+  arr = _arr;
+  
+}
+void show_data()
+{
+    cout << "Size: " << SIZE << endl;
+
+    for(size_t i=0; i<=SIZE;++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+~A()
+{
+    delete[] arr;
+    arr = nullptr;
+}
+
+
+};
+
+void foo()
+{
+int b;
+
+}
 
 int main()
 {
+    int N = 5;
+    int array[N] = {4,7,3,5,1};
+
+    A a(array, N);
+    a.show_data();
 
     return 0;
 }
