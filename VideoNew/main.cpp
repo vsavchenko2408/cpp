abba0b592e26e7cfb9f18dda76e78e0d457d0c56
DerMeister <v.savchenko2408@gmail.com>
2024-10-01 22:59:27 +0300

    51 video

--- VideoNew/main.cpp
+++ VideoNew/main.cpp
@@ -1,44 +1,37 @@
 #include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
 
 class Parent
 {
-    int m_data;
-
 public:
-    Parent() : m_data(0) {}
-    Parent(int data) : m_data(data) {}
-    void set_data(int data)
-    {
-        m_data = data;
-    }
-    int get_data()
-    {
-        return m_data;
-    }
+    string msgOne = "Public";
+
+protected:
+    string msgTwo = "Protected";
+
+private:
+    string msgThree = "Protected";
 };
 
-class Child : public Parent
+class Child : private Parent
 {
-    int c_data;
 
 public:
-    Child() : c_data(0), Parent() {}
-    Child(int ch_data, int p_data) : c_data(ch_data), Parent(p_data) {}
-
-    void show_data()
+    void show_msg()
     {
-        std::cout << "C_data: " << c_data << std::endl;
-        std::cout << "P_data: " << get_data() << std::endl;
+        cout << msgOne << endl;
+        cout << msgTwo << endl;
     }
 };
 
 int main()
 {
-
-    Child c(14, 25);
-    c.show_data();
-    c.set_data(500);
-    c.show_data();
-
+    Child obj;
+    //obj.msgOne;
+    //obj.msgTwo;
+    obj.show_msg();
     return 0;
 }
