afff4c526a6fd6f356d6330202fa7b43291309ed
DerMeister <v.savchenko2408@gmail.com>
2024-10-09 22:29:36 +0300

    Virtual Methods and Polimorphism

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,11 +1,57 @@
 #include <iostream>
-
+// ad-hoc polimorphism
 using std::cin;
 using std::cout;
 using std::endl;
 
+class Enemy
+{
+public:
+  virtual void attack() = 0;
+};
+
+class Warrior : public Enemy
+{
+public:
+  void attack() override
+  {
+    cout << "Sword!" << endl;
+  }
+};
+
+class Mage : public Enemy
+{
+public:
+  void attack() override
+  {
+    cout << "Fireball!" << endl;
+  }
+};
+
+class Archer : public Enemy
+{
+public:
+  void attack() override
+  {
+    cout << "Bow!" << endl;
+  }
+};
 int main()
 {
-
+  Warrior w;
+  Archer a;
+  Mage m;
+  Enemy *ptr = &w;
+  // ptr->attack();
+  Enemy *enemies[3];
+  enemies[0] = &w;
+  enemies[1] = &a;
+  enemies[2] = &m;
+  for (int i = 0; i < 3; ++i)
+  {
+    ptr = enemies[i];
+    ptr->attack();
+  }
+  Enemy e;
   return 0;
 }
