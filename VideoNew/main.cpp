02e67a41f34f0505f9ac965621645b0b7b656f0a
DerMeister <v.savchenko2408@gmail.com>
2024-10-09 16:43:55 +0300

    VideoNew

--- VideoNew/main.cpp
+++ VideoNew/main.cpp
@@ -1,46 +1,41 @@
 #include <iostream>
-#include <string>
+#include <algorithm>
+#include <random>
 using std::cout;
 using std::endl;
 using std::string;
-/*
-FILO
-First input
-Last output
-*/
-class Parent
+
+bool cmp(int &a, int &b)
 {
-public:
-    Parent()
-    {
-        cout << __PRETTY_FUNCTION__ << endl;
-    }
+    return a>b;
+}
 
-    ~Parent()
-    {
-        cout << __PRETTY_FUNCTION__ << endl;
-    }
-};
 
-class Child : public Parent
+void fillarray(int *arr, const int N)
 {
-public:
-    Child()
+    for (int i = 0; i < N; ++i)
     {
-        cout << __PRETTY_FUNCTION__ << endl;
+        arr[i] = rand() % 99;
     }
+}
 
-    ~Child()
+void showarr(int *arr, const int N)
+{
+    for (int i = 0; i < N; ++i)
     {
-        cout << __PRETTY_FUNCTION__ << endl;
+        cout << arr[i] << " ";
     }
-};
+    cout << endl;
+}
 
 int main()
 {
-   // Parent p;
-    Child c;
-    Parent p;
-
+    bool(*ptrcmp)(int &, int &) = cmp;
+    const int SIZE = 50;
+    int *arr = new int[SIZE];
+    fillarray(arr, SIZE);
+    showarr(arr, SIZE);
+    std::sort(arr,arr+SIZE,ptrcmp);
+    showarr(arr, SIZE);
     return 0;
 }
