ed55967c0710ccfd433c962888caacf00e9d5a0a
DerMeister <v.savchenko2408@gmail.com>
2024-10-09 18:26:22 +0300

    Remove VideoNew and Clear Video

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,41 +1,11 @@
 #include <iostream>
-#include <string>
-#include "Pointers.h"
+
 using std::cin;
 using std::cout;
 using std::endl;
-using std::string;
-
-int Pointers::count = 0;
-
-void zero_coords(Pointers &obj)
-{
-  obj.x = 0;
-  obj.y = 0;
-}
 
 int main()
 {
 
-  Pointers p1(3, 7);
-  Pointers p2(4, 7);
-  cout << p1 * p2 << p1 + p2 << endl;
-
-  /*
-  if(p1 > p2)
-  {
-    cout << "Equil!"  << endl;
-  }
-  else
-  {
-      cout << "Not Equil!" << endl;
-  }
-  */
-
-  // zero_coords(p1);
-  // p1.show();
-  // cout << Pointers::count << endl;
-
-  // cout << Pointers::count << endl;
   return 0;
 }
