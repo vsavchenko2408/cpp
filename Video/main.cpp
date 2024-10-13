2ddc7577b885ef562675d43fe781d327ded911b7
DerMeister <v.savchenko2408@gmail.com>
2024-10-13 22:53:55 +0300

    Diamond Inheritance

--- Video/main.cpp
+++ Video/main.cpp
@@ -7,34 +7,27 @@ using std::endl;
 class Parent
 {
 public:
-Parent()
-{
-  cout 
-}
-void show()
-{
-  cout << "Parent!" << endl;
-}
+  void show()
+  {
+    cout << "Parent!" << endl;
+  }
 };
 
 class Child1 : virtual public Parent
 {
-
 };
 
 class Child2 : virtual public Parent
 {
-
 };
 
 class Grandson : public Child1, public Child2
 {
-
 };
 int main()
 {
 
-Grandson g;
-g.show();
+  Grandson g;
+  g.show();
   return 0;
 }
