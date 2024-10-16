e1532c8de833ec27c936176574d18a9e84ed1e37
DerMeister <v.savchenko2408@gmail.com>
2024-10-16 19:35:29 +0300

    clean Video

--- Video/main.cpp
+++ Video/main.cpp
@@ -4,38 +4,8 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-class Parent
-{
-public:
-  Parent()
-  {
-    cout << "Constr Parent" << endl;
-  }
-  virtual ~Parent()
-  {
-    cout << "Destr Parent" << endl;
-  }
-};
-
-class Child : public Parent
-{
-  public:
-  Child()
-  {
-    cout << "Constr Child" << endl;
-  }
-  ~Child()
-  {
-    cout << "Destr Child" << endl;
-  }
-};
-
-
 int main()
 {
 
-Parent* ptr = new Child();
-
-delete ptr;
   return 0;
 }
