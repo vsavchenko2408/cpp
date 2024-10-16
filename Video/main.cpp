91951d21e502e693e97aee050725807e97524641
DerMeister <v.savchenko2408@gmail.com>
2024-10-16 20:32:30 +0300

    files

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using std::cin;
 using std::cout;
@@ -6,6 +7,19 @@ using std::endl;
 
 int main()
 {
+  std::fstream file;
+  file.open("MyFile.txt", std::ios::out | std::ios::app);
 
+  std::string str = "some text!";
+  if (!file.is_open())
+  {
+    std::cerr << "File not open!" << endl;
+  }
+  else
+  {
+    file << str;
+  }
+
+  file.close();
   return 0;
 }
