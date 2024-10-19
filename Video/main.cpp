55a238d0af9e37b35620fdd6a423c986295b5c46
DerMeister <v.savchenko2408@gmail.com>
2024-10-20 01:20:12 +0300

    read from file

--- Video/main.cpp
+++ Video/main.cpp
@@ -7,19 +7,42 @@ using std::endl;
 
 int main()
 {
-  std::fstream file;
-  file.open("MyFile.txt", std::ios::out | std::ios::app);
-
-  std::string str = "some text!";
-  if (!file.is_open())
+  ///////////
+  std::ofstream ofile("MyFile.txt");
+  std::string ostr = "some text!";
+  if (!ofile.is_open())
   {
-    std::cerr << "File not open!" << endl;
+    std::cerr << "File not open for write!" << endl;
   }
   else
   {
-    file << str;
+    cout << "File opened and write!" << endl;
+    ofile << ostr;
   }
-
-  file.close();
+  ofile.close();
+  ////////////
+  std::ifstream ifile("MyFile.txt");
+  // std::string istr;
+  char c;
+  char str[50];
+  int count = 0;
+  if (!ifile.is_open())
+  {
+    std::cerr << "File nor open for read!" << endl;
+  }
+  else
+  {
+    cout << "File opened and read!" << endl;
+    while (ifile.get(c))
+    {
+      str[count] = c;
+      count++;
+    }
+    str[count] = '\0';
+    //  std::getline(ifile, istr);
+  }
+  cout << str << "\n";
+  // cout << istr << endl;
+  ifile.close();
   return 0;
 }
