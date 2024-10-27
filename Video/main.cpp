4593490deb8918e599db61c45210d19a6e933348
DerMeister <v.savchenko2408@gmail.com>
2024-10-27 17:52:05 +0200

    Abstract class

--- Video/main.cpp
+++ Video/main.cpp
@@ -4,8 +4,46 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+class Weapon
+{
+public:
+virtual void shoot() = 0;
+};
+
+class Pistol : public Weapon
+{
+  public:
+void shoot() override
+{
+  cout << "Bang!" << std::endl;
+}
+};
+class Machinegun : public Weapon
+{
+  public:
+void shoot() override
+{
+  cout << "Bangbangbang!" << std::endl;
+}
+};
+
+class Player
+{
+  public:
+  void attack(Weapon* w)
+  {
+    w->shoot();
+  }
+};
+
 int main()
 {
+Pistol p;
+Machinegun m;
+Weapon* ptr = &p; 
+Player pt;
+
+pt.attack(ptr);
 
   return 0;
 }
