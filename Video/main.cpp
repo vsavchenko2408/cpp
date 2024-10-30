cfd8b795977b941fb0e226da757cfada421dd32a
Viacheslav <v.savchenko2408@gmail.com>
2024-10-30 06:44:24 +0200

    clear video

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,49 +1,13 @@
 #include <iostream>
-#include <ctime>
+
 
 using std::cin;
 using std::cout;
 using std::endl;
-class Watch
-{
-public:
-  void time()
-  {
-    cout << "Time!" << endl;
-  }
-};
 
-class Human
-{
-  class Brain
-  {
-  public:
-    void think()
-    {
-      cout << "I am think!" << endl;
-    }
-  };
-  Brain b;
-
-public:
-  void seetime(Watch *w)
-  {
-    w->time();
-  }
-  void Think()
-  {
-    b.think();
-  }
-};
 
 int main()
 {
-  Watch w;
-  Human h;
-  Human h2;
-  h2.seetime(&w);
-  h.seetime(&w);
-  // h.Think();
 
   return 0;
 }
