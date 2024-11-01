47444318365ed13a97d398a842a09247a9c2c9ea
DerMeister <v.savchenko2408@gmail.com>
2024-11-01 16:11:04 +0200

    Clear Video

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-#include "MyClass.h"
 
 using std::cin;
 using std::cout;
 using std::endl;
 
-
 int main()
 {
-  MyClass<int> mci(15);
-  MyClass<double> mcd;
-  mcd.set_data(43.78);
-  cout << mci;
-  cout << mcd;
 
   return 0;
 }
