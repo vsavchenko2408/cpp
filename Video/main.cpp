82cfec535495053c48ed5a6f56e5149cc799cfe5
DerMeister <v.savchenko2408@gmail.com>
2024-11-01 16:34:53 +0200

    template Array Video

--- Video/main.cpp
+++ Video/main.cpp
@@ -4,8 +4,38 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+template <class T>
+class Array
+{
+int m_length;
+T* arr;
+public:
+Array()
+{
+  m_length = 0;
+  arr = nullptr;
+}
+Array(int length)
+{
+  m_length = length;
+  arr = new T[m_length];
+}
+
+~Array()
+{
+  delete[] arr;
+  arr = nullptr;
+}
+T& operator[](int index)
+{
+  return arr[index];
+}
+
+};
 int main()
 {
-
+Array<double> arr(10);
+arr[5] = 10.10;
+cout << arr[5] << endl;
   return 0;
 }
