3de240fac6e83d0ff903076e2d631598d898d1d3
DerMeister <v.savchenko2408@gmail.com>
2024-11-04 10:04:38 +0200

    trycatch

--- trycatch/main.cpp
+++ trycatch/main.cpp
@@ -6,6 +6,42 @@ using std::endl;
 
 int main()
 {
+    int i = 10;
+    float f = 4.3f;
+    bool b = true;
+    double d = 65.3434;
+    char c = 'A';
+    std::string str = "Error!";
+    try
+    {
+
+        throw str;
+    }
+    catch (int)
+    {
+        cout << "Integer: " << i << endl;
+    }
+    catch (float)
+    {
+        cout << "Float: " << f << endl;
+    }
+    catch (double)
+    {
+        cout << "Double: " << d << endl;
+    }
+    catch (char)
+    {
+        cout << "Integer: " << c << endl;
+    }
+    catch (std::string)
+    {
+        cout << "String: " << str << endl;
+    }
+    catch (...)
+    {
+
+        cout << "Unknown Error! " << endl;
+    }
 
     return 0;
 }
