1acd4eef54bf4c2dfb9fac94062f8e370c20bc87
DerMeister <v.savchenko2408@gmail.com>
2024-11-04 09:38:35 +0200

    clear trycatch

--- trycatch/main.cpp
+++ trycatch/main.cpp
@@ -1,61 +1,11 @@
 #include <iostream>
-#include <vector>
+
 using std::cin;
 using std::cout;
 using std::endl;
 
-template <class T1, class T2>
-T1 devide(T1 a, T2 b)
-{
-   int* ptr = new int(15);
-    if(b == 0)
-    {
-        throw b;
-    }
-    else if(a == 5)
-    {
-        throw a;
-    }
-    else 
-    delete ptr;
-    return a/b;
-}
-
 int main()
 {
-
-try
-{
-    int sc;
-    cout << devide(5,0) << endl;
-}
-catch( int err)
-{
-    std::cerr << "Integer: " << err << '\n';
-}
-    catch (float err)
-{
-        cout << "Float! " << err << endl;
-}
-    catch (double err)
-{
-        cout << "Double! " << err << endl;
-}
-catch(void* ptr)
-{
-    cout << "Ptr" << ptr << endl;
-}
-
-
-catch(...)
-{
-    cout << "Error!" << endl;
-}
-
-
-
-
-
 
     return 0;
 }
