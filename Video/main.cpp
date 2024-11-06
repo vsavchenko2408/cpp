0b0ec75ca64750f2f0956e72b3c65f9a8f369c83
DerMeister <v.savchenko2408@gmail.com>
2024-11-06 17:08:53 +0200

    clear Other

--- Video/main.cpp
+++ Video/main.cpp
@@ -3,12 +3,49 @@
 using std::cin;
 using std::cout;
 using std::endl;
-// compile error
-// runtime error
+// exception == вийняток
 
 
 int main()
 {
+int i;
+float f;
+double d;
+char c;
+bool b;
+std::string str;
+try 
+{
+   throw d;
+}
+
+catch(float)
+{
+  std::cout << "Float! " << endl;
+}
+catch(double)
+{
+  std::cout << "Double! " << endl;
+}
+catch(int)
+{
+  std::cout << "Integer! " << endl;
+}
+catch(char)
+{
+  std::cout << "Char! " << endl;
+}
+catch(bool)
+{
+  std::cout << "Bool! " << endl;
+}
+catch(...)
+{
+    cout << "Unknown Error!" << endl;
+}
+
+
+
 
     return 0;
 }
--- other/main.cpp
+++ other/main.cpp
@@ -1,22 +1,9 @@
 #include <iostream>
 
-class A
-{
-int x=5;
-int y=6;
-public:
-friend void f(A& obj);
-
-};
-
-void f(A& obj)
-{
-   std::cout <<  obj.x << std::endl;
-   std::cout <<  obj.y << std::endl;
-}
 
 int main()
 {
-A a;
-f(a);
+
+
+	
 }
