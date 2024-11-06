8802ed9e26e8b87896c090580e67791d35ea46a8
DerMeister <v.savchenko2408@gmail.com>
2024-11-06 17:35:01 +0200

    other

--- other/main.cpp
+++ other/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 
+class MyClass
+{
+	public:
+		int* ptr;
+		MyClass() 
+		{
+			ptr = nullptr;
+			std::cout << __PRETTY_FUNCTION__ << std::endl;
+		}
+		MyClass(long long length)
+		{
+			ptr = new int[length];
+			std::cout << __PRETTY_FUNCTION__ << std::endl;
+		}
+		~MyClass()
+		{
+		std::cout << __PRETTY_FUNCTION__ << std::endl;
+		delete[] ptr;
+		ptr = nullptr;
+		}
 
+};
 int main()
 {
-
-
+	try
+	{
+MyClass mc(400'000'000'000);
+	throw &mc;
+	}
+	catch(...)
+	{
+	std::cout << "Caught!" << std::endl;
+	}
 	
 }
