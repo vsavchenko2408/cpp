79f3e38f3f8fbb5b3b4c9679ed3a1f3ca625d441
DerMeister <v.savchenko2408@gmail.com>
2024-11-08 11:21:47 +0200

    SFMLtest

--- SFMLtest/main.cpp
+++ SFMLtest/main.cpp
@@ -1,17 +1,31 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
-using namespace sf;
 int main()
 {
-sf::Window game(VideoMode(800,600), "Test window");
-while(game.isOpen())
-{
-    Event event;
+    // Створюємо вікно з можливістю малювання
+    sf::RenderWindow game(sf::VideoMode(800, 600), "Test window");
 
+    // Цикл, поки вікно відкрите
+    while (game.isOpen())
+    {
+        sf::Event event;
+        while (game.pollEvent(event))
+        {
+            // Закриваємо вікно, якщо натиснута кнопка закриття
+            if (event.type == sf::Event::Closed)
+                game.close();
+        }
 
-    game.display();
-}
+        game.clear(sf::Color::White);
+        sf::CircleShape circle(50.0f);
+        circle.setFillColor(sf::Color::Black);
+        circle.move(15, 15);
+        game.draw(circle);
 
-return 0;
+        // Відображаємо все, що було намальовано на екрані
+        game.display();
+    }
+
+    return 0;
 }
