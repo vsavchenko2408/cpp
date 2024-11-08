9e11d7f2c5811f2ca82874db7942e1126848d70a
DerMeister <v.savchenko2408@gmail.com>
2024-11-08 19:58:37 +0200

    clear Video

--- Video/main.cpp
+++ Video/main.cpp
@@ -8,41 +8,6 @@ using std::endl;
 
 int main()
 {
-int i;
-float f;
-double d;
-char c;
-bool b;
-std::string str;
-try 
-{
-   throw d;
-}
-
-catch(float)
-{
-  std::cout << "Float! " << endl;
-}
-catch(double)
-{
-  std::cout << "Double! " << endl;
-}
-catch(int)
-{
-  std::cout << "Integer! " << endl;
-}
-catch(char)
-{
-  std::cout << "Char! " << endl;
-}
-catch(bool)
-{
-  std::cout << "Bool! " << endl;
-}
-catch(...)
-{
-    cout << "Unknown Error!" << endl;
-}
 
 
 
