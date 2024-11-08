ea20bca2ca662c8508f2a1e7adb2faa999b25cf8
DerMeister <v.savchenko2408@gmail.com>
2024-11-08 19:38:39 +0200

    trycatch

--- trycatch/main.cpp
+++ trycatch/main.cpp
@@ -4,45 +4,9 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+
 int main()
 {
-    int i = 10;
-    float f = 4.3f;
-    bool b = true;
-    double d = 65.3434;
-    char c = 'A';
-    std::string str = "Error!";
-    std::exception ex;
-    try
-    {
-
-        throw ex;
-    }
-    catch (int)
-    {
-        cout << "Integer: " << i << endl;
-    }
-    catch (float)
-    {
-        cout << "Float: " << f << endl;
-    }
-    catch (double)
-    {
-        cout << "Double: " << d << endl;
-    }
-    catch (char)
-    {
-        cout << "Integer: " << c << endl;
-    }
-    catch (std::string)
-    {
-        cout << "String: " << str << endl;
-    }
-    catch (...)
-    {
-
-        cout << "Unknown Error! " << endl;
-    }
 
     return 0;
 }
