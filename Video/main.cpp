04dec1011e7fcb76f95bca2f805feeb3a7022995
DerMeister <v.savchenko2408@gmail.com>
2024-11-08 22:12:59 +0200

    Video

--- Video/main.cpp
+++ Video/main.cpp
@@ -5,9 +5,36 @@ using std::cout;
 using std::endl;
 // exception == вийняток
 
+class MyExc : public std::exception
+{
+private:
+std::string errmsg;
+public:
+MyExc(const std::string msg) : errmsg(msg)
+{}
+const char* what() const noexcept override 
+{
+  return errmsg.c_str();
+}
+
+};
 
 int main()
 {
+
+try
+{
+  void f();
+  throw MyExc("Failed allocate memory!");
+  void g();
+  
+}
+catch(const MyExc& e)
+{
+
+  std::cerr <<  "Error! " << e.what() << '\n';
+}
+
 
 
 
