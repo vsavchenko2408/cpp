98dd7db20d06e5e1fa2a096ecbc3c1e97ea9f0c3
DerMeister <v.savchenko2408@gmail.com>
2024-11-13 00:29:53 +0200

    typedef size_t and int16_t

--- Video/main.cpp
+++ Video/main.cpp
@@ -3,9 +3,18 @@
 using std::cin;
 using std::cout;
 using std::endl;
+typedef unsigned long long ull;
 
 int main()
 {
 
+  cout << "int8_t: " << sizeof(int8_t) << endl;
+  cout << "int16_t: " << sizeof(int16_t) << endl;
+  cout << "int32_t: " << sizeof(int32_t) << endl;
+  cout << "int64_t: " << sizeof(int64_t) << endl;
+  cout << "u_int8_t: " << sizeof(u_int8_t) << endl;
+  cout << "u_int16_t: " << sizeof(u_int16_t) << endl;
+  cout << "u_int32_t: " << sizeof(u_int32_t) << endl;
+  cout << "u_int64_t: " << sizeof(u_int64_t) << endl;
   return 0;
 }
