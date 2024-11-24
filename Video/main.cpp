9615c863bc7b395cdb4ad31ebc456b3a0ef0f3f3
DerMeister <v.savchenko2408@gmail.com>
2024-11-25 00:47:21 +0200

    smartpointer

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,70 +1,31 @@
 #include <iostream>
-#include <algorithm>
-#include <random>
-
+#include <memory>
 using std::cin;
 using std::cout;
 using std::endl;
-void fillarray(int *ptr, const size_t N);
-void showarray(int *ptr, const size_t N);
-void swap(int &a, int &b);
-//void sort(int *arr, const int N);
-bool comp(int& a, int& b);
+
+template <typename T>
+class SmartPointer
+{
+private:
+  T *m_ptr;
+
+public:
+  SmartPointer(T *ptr) : m_ptr(ptr) {}
+  T &operator*()
+  {
+    return *m_ptr;
+  }
+  ~SmartPointer()
+  {
+    delete m_ptr;
+  }
+};
 
 int main()
 {
-  const size_t SIZE = 20;
-  srand(time(NULL));
-  int *arr = new int[SIZE];
-  fillarray(arr, SIZE);
-  showarray(arr, SIZE);
-  //sort(arr, SIZE);
-  std::sort(arr, arr+SIZE,comp);
-  cout << "Sorted!" << endl;
-  showarray(arr, SIZE);
-  
+  SmartPointer<int> ptr = new int(10);
+  cout << *ptr << endl;
+
   return 0;
 }
-
-void showarray(int *ptr, const size_t N)
-{
-  for (size_t i = 0; i < N; ++i)
-  {
-    cout << ptr[i] << " ";
-  }
-  cout << endl;
-}
-
-void fillarray(int *ptr, const size_t N)
-{
-  for (size_t i = 0; i < N; ++i)
-  {
-    ptr[i] = rand() % 1000;
-  }
-}
-
-void swap(int &a, int &b)
-{
-  int temp = a;
-  a = b;
-  b = temp;
-}
-bool comp(int& a, int& b)
-{
-return a>b;
-}
-/*
-void sort(int *arr, const int N)
-{
-  for (size_t i = 0; i < N -1 ; ++i)
-  {
-    for (size_t j = i +1 ; j < N; ++j)
-    {
-      if (arr[i] > arr[j])
-      {
-        swap(arr[i], arr[j]);
-      }
-    }
-  }
-}
-*/
