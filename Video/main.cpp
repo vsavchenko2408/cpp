4ed8e244e5c1ed99ea73f3652836fafbffcf2fa4
DerMeister <v.savchenko2408@gmail.com>
2024-11-28 03:10:46 +0200

    Clear Video

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,40 +1,11 @@
 #include <iostream>
-#include <memory>
+
 using std::cin;
 using std::cout;
 using std::endl;
-// auto_ptr
-// unique_ptr
-// shared_ptr
-
-template <typename T>
-class SmartPointer
-{
-private:
-  T *m_ptr;
-
-public:
-  SmartPointer(T *ptr) : m_ptr(ptr) {}
-  SmartPointer(const SmartPointer&) = delete;
-  SmartPointer& operator=(const SmartPointer&) = delete;
-  T &operator*()
-  {
-    return *m_ptr;
-  }
-  ~SmartPointer()
-  {
-    delete m_ptr;
-  }
-};
 
 int main()
 {
-
-
-
-  std::shared_ptr<int> sptr(new int(14));
-  std::shared_ptr<int> sptr2 = sptr;
-  cout << *sptr << endl;
 
   return 0;
 }
