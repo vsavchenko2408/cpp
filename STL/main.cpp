a93c08932c535336f6279e23848b957e35e7328b
DerMeister <v.savchenko2408@gmail.com>
2024-12-10 06:14:09 +0200

    add system() in STL Video

--- STL/main.cpp
+++ STL/main.cpp
@@ -6,14 +6,16 @@ using std::vector;
 int main()
 {
 
-vector<int> MyVec = {14,35,32,98,67,94,9};
-MyVec.push_back(17);
-MyVec.erase(MyVec.begin()+1,MyVec.begin()+4);
-for(size_t i =0;i<MyVec.size();++i)
-{
-    cout << MyVec[i] << endl;
-}
+    vector<int> MyVec = {14, 35, 32, 98, 67, 94, 9};
+    MyVec.push_back(17);
+    MyVec.erase(MyVec.begin() + 1, MyVec.begin() + 4);
+    for (size_t i = 0; i < MyVec.size(); ++i)
+    {
+        cout << MyVec[i] << endl;
+    }
 
-
+#ifndef WIN32
+    system("pause");
+#endif
     return 0;
 }
