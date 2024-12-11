87087ef555e004997de1912ad6e6ea73bb689750
DerMeister <v.savchenko2408@gmail.com>
2024-12-11 18:47:15 +0200

    clear STL

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,21 +1,9 @@
 #include <iostream>
-#include <vector>
 using std::cout;
 using std::endl;
-using std::vector;
+
 int main()
 {
 
-    vector<int> MyVec = {14, 35, 32, 98, 67, 94, 9};
-    MyVec.push_back(17);
-    MyVec.erase(MyVec.begin() + 1, MyVec.begin() + 4);
-    for (size_t i = 0; i < MyVec.size(); ++i)
-    {
-        cout << MyVec[i] << endl;
-    }
-
-#ifndef WIN32
-    system("pause");
-#endif
     return 0;
 }
