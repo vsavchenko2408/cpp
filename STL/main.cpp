df3dc0003bf861439d058ce32e8e46f0e80b7b90
DerMeister <v.savchenko2408@gmail.com>
2024-12-13 10:56:21 +0200

    iterators

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
-
+#include <forward_list>
 using std::cout;
 using std::endl;
+using std::forward_list;
 
 int main()
 {
+    forward_list<int> v{10, 20, 30, 40, 50};
+    forward_list<int>::iterator it = v.begin();
 
+    for (auto i: v)
+    {
+        cout << *it << endl;
+        ++it;
+    }
     return 0;
 }
