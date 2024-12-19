0d4159242b074d251e49034d4066e92161f33ff5
DerMeister <v.savchenko2408@gmail.com>
2024-12-19 09:44:58 +0200

    clear STL

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
-#include <list>
 using std::cout;
-using std::list;
 using std::endl;
 
 int main()
 {
-    list<int> v{10, 20, 30, 40, 50};
-    list<int>::iterator it = v.begin();
-    
-    for (size_t i =0; i < v.size(); ++i)
-    {
-        cout << *it << endl;
-        ++it;
-    }
+
     return 0;
 }
