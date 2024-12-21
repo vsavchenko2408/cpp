8c93a374429d5d06f2943f291ba571bae86ad6f6
DerMeister <v.savchenko2408@gmail.com>
2024-12-21 09:39:33 +0200

    STL deque

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
-
+#include <deque>
+#include <vector>
+// double ended queue
 using std::cout;
+using std::deque;
 using std::endl;
+using std::vector;
 
 int main()
 {
+
+    size_t size = 0;
+    std::cin >> size;
+    deque<int> v(size);
+    int count =0;
+    while(!v.empty())
+    {
+        count++;
+        v.erase(v.begin());
+        cout << "count: " << count << endl;
+    }
+
+
+    //  deque<int> dq;
 
     return 0;
 }
