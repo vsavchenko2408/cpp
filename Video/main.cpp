8dacc53b9f287e7155d13f2922b026d6d8fbd0c0
DerMeister <v.savchenko2408@gmail.com>
2024-12-23 08:44:57 +0200

    clear Video

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-#include <set>
 
 using std::cin;
 using std::cout;
-using std::set;
 using std::endl;
 
 int main()
 {
-  set<double> dq{43, 23, 87, 76, 56, 45};
-  for(auto it : dq)
-  {
-    cout << it << endl;
-  }
 
   return 0;
 }
