f88cf527b60083721e93b98652d14da7e9b48de8
DerMeister <v.savchenko2408@gmail.com>
2024-12-24 22:19:25 +0200

    other

--- other/main.cpp
+++ other/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <unistd.h>
 
 int main()
 {
-
-	
+    size_t count = 0;
+    while (count != 1000)
+    {
+        std::cout << "1";
+        usleep(10000);
+        std::system("clear");
+        std::cout << '2';
+        usleep(10000);
+        std::system("clear");
+        // std::cout << '\n';
+        std::cout << '3';
+        usleep(10000);
+        std::system("clear");
+        // std::cout << '\n';
+        std::cout << '4';
+        usleep(10000);
+        std::system("clear");
+        count++;
+    }
+    return 0;
 }
