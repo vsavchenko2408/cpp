b2398d327c500dce47f93f104b8311413bfa490f
DerMeister <v.savchenko2408@gmail.com>
2024-12-27 10:25:42 +0200

    set multiset

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <set>
 
 using std::cout;
 using std::endl;
+using std::multiset;
 
 int main()
 {
+    multiset<int> st{10, 15, 17, 16};
+    st.insert(7);
+    st.emplace(5);
+    st.insert(10);
+    st.emplace(10);
 
+    for (auto i : st)
+    {
+        cout << i << endl;
+    }
+    cout << st.count(10) << endl;
     return 0;
 }
