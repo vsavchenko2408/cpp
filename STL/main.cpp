60e23fc7b99e8afc272a166875e5dd03f3f38757
DerMeister <v.savchenko2408@gmail.com>
2024-12-29 08:22:15 +0200

    STL

--- STL/main.cpp
+++ STL/main.cpp
@@ -3,20 +3,18 @@
 
 using std::cout;
 using std::endl;
-using std::multiset;
+using std::set;
 
 int main()
 {
-    multiset<int> st{10, 15, 17, 16};
+    set<int> st{12, 10, 15, 17, 16};
     st.insert(7);
     st.emplace(5);
     st.insert(10);
-    st.emplace(10);
-
+    // set<int>::iterator it = st.begin();
     for (auto i : st)
     {
         cout << i << endl;
     }
-    cout << st.count(10) << endl;
     return 0;
 }
