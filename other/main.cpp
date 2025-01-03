e9586509bf35239e13792ee32580e87647def4c2
DerMeister <v.savchenko2408@gmail.com>
2025-01-03 15:02:49 +0200

    other

--- other/main.cpp
+++ other/main.cpp
@@ -10,16 +10,24 @@ int main()
     {
         for(size_t j = 0; j < M; ++j)
         {
-            arr[i][j] = i*M+j;
+            arr[i][j] = i*M+j+1;
         }
     }
+    /*
     for(size_t i =0;i<N;++i)
     {
         for(size_t j = 0; j < M; ++j)
         {
-           std::cout <<  arr[i][j];
+           std::cout <<  &arr[i][j] << " ";
         }
         std::cout << std::endl;
+    }
+    */
+    std::cout << std::endl;
+    int *p = reinterpret_cast<int*> (arr);
+    for(int k = 0; k < N*M; ++k)
+    {
+        std::cout << *(p + k) << ' ';
     }
 
 
