fd7ff3e41130468d5c728751e112957aba4fb87c
DerMeister <v.savchenko2408@gmail.com>
2025-01-08 11:28:54 +0200

    clear STL

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
-#include <set>
 
 using std::cout;
 using std::endl;
-using std::set;
 
 int main()
 {
-    set<int> st{12, 10, 15, 17, 16};
-    st.insert(7);
-    st.emplace(5);
-    st.insert(10);
-    // set<int>::iterator it = st.begin();
-    for (auto i : st)
-    {
-        cout << i << endl;
-    }
+
     return 0;
 }
