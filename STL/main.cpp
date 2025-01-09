a34e6903878b39a60aede1558064359c8758e787
DerMeister <v.savchenko2408@gmail.com>
2025-01-09 09:44:14 +0200

    Clear STL

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,33 +1,7 @@
 #include <iostream>
-#include <map>
-#include <string>
-
-using std::cout;
-using std::endl;
-using std::multimap;
-using std::string;
-
-void show(multimap<string, unsigned int> pns);
 
 int main()
 {
-    multimap<string, unsigned int> pns;
-
-    pns.emplace("Vasyl", 38097345);
-    pns.emplace("Maria", 38097567);
-    pns.emplace("Volodymir", 38097123);
-    pns.emplace("Hanna", 38097234);
-    pns.emplace("Hanna", 38095234);
-
-    show(pns);
 
     return 0;
-}
-
-void show(multimap<string, unsigned int> pns)
-{
-    for (auto i : pns)
-    {
-        cout << "Name: " << i.first << " - " << "Number: " << i.second << endl;
-    }
 }
