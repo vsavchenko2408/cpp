293cf97f6c21988de5ad00694c456719a0fa5239
DerMeister <v.savchenko2408@gmail.com>
2025-01-12 00:13:38 +0200

    clear STL

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,28 +1,10 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-#include <list>
 
 using std::cout;
 using std::endl;
-using std::list;
-using std::priority_queue;
-using std::vector;
 
 int main()
 {
-    priority_queue<int> q;
-    q.emplace(5);
-    q.emplace(43);
-    q.emplace(32);
-    q.emplace(15);
-    q.emplace(22);
-
-    while (!q.empty())
-    {
-        cout << q.top() << endl;
-        q.pop();
-    }
 
     return 0;
 }
