f3969595ffa305441d1de5fa60e0ed0e09ca9b0d
DerMeister <v.savchenko2408@gmail.com>
2025-01-12 21:04:27 +0200

    search()

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-
+#include <algorithm>
+#include <vector>
 using std::cout;
 using std::endl;
+using std::search;
+using std::vector;
 
 int main()
 {
+    vector<int> arr = {1,2,3,4,5,6,5,8,9,10};
+    int srch[3] = { 8,9,10};
+    auto ptr = search(arr.begin(),arr.end(),srch, srch+3);
 
+    cout << *ptr << endl;
     return 0;
 }
