52d065593ef6ccd730db97074a9b9ab2f250c9dc
DerMeister <v.savchenko2408@gmail.com>
2025-01-13 22:05:14 +0200

    other multithread

--- other/main.cpp
+++ other/main.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
 #include <thread>
-
+#include <chrono>
+#include <future>
 void dowork1()
 {
+    using namespace std::chrono_literals;
     while (true)
     {
-        std::cout << "First thread" << std::endl;
+        std::cout << "First thread: " << std::this_thread::get_id << std::endl;
+        std::this_thread::sleep_for(1000ms);
     }
 }
 
 void dowork2()
 {
+    using namespace std::chrono_literals;
     while (true)
     {
-        std::cout << "Second thread" << std::endl;
+        std::cout << "Second thread:  " << std::this_thread::get_id << std::endl;
+        std::this_thread::sleep_for(1000ms);
     }
 }
 
 int main()
 {
-    thread td1(dowork1);
-    thread td2(dowork2);
 
-    td1.join();
-    td2.join();
+    // std::thread td1(dowork1);
 
+    // std::thread td2(dowork2);
+
+    // td1.join();
+    // td2.join();
+    std::async(dowork1);
+    std::async(dowork2);
     return 0;
 }
