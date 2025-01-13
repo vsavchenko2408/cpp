4b3a8c9074f38fb64eb26979ba734b7b607125cc
DerMeister <v.savchenko2408@gmail.com>
2025-01-13 18:23:22 +0200

    other multithread

--- other/main.cpp
+++ other/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <thread>
+
+void dowork1()
+{
+    while (true)
+    {
+        std::cout << "First thread" << std::endl;
+    }
+}
+
+void dowork2()
+{
+    while (true)
+    {
+        std::cout << "Second thread" << std::endl;
+    }
+}
 
 int main()
 {
+    thread td1(dowork1);
+    thread td2(dowork2);
+
+    td1.join();
+    td2.join();
 
     return 0;
 }
