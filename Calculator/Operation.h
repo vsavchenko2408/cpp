d9709e04c6357278440eb7492fecebbd605623b8
DerMeister <v.savchenko2408@gmail.com>
2025-01-14 09:11:10 +0200

    Calculator

--- Calculator/Operation.h
+++ Calculator/Operation.h
@@ -1,42 +1,38 @@
 #ifndef OPERATION_H
 #define OPERATION_H
+#include <iostream>
 
-
-template<typename T1, typename T2>
+template <typename T1, typename T2>
 class Operation
 {
-T1 left;
-T2 right;
+    T1 left;
+    T2 right;
+
 public:
+    Operation()
+    {
+        std::cout << "Enter first value: ";
+        std::cin >> left;
+        std::cout << "Enter second value: ";
+        std::cin >> right;
+    }
 
-Operation()
-{
-std::cout << "Enter first value: ";
-std::cin >> left;
-std::cout << "Enter second value: ";
-std::cin >> right;
-}
-
-
-T1 plus()//adding
-{
-    return left+right;
-}
-T1 minus()//subtracting
-{
-    return left-right;
-}
-T1 multi()//multiple
-{
-    return left*right;
-}
-T1 divis()//division
-{
-    return left/right;
-}
-
-
+    T1 plus() // adding
+    {
+        return left + right;
+    }
+    T1 minus() // subtracting
+    {
+        return left - right;
+    }
+    T1 multi() // multiple
+    {
+        return left * right;
+    }
+    T1 divis() // division
+    {
+        return left / right;
+    }
 };
-
 
 #endif
--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -5,50 +5,39 @@ void Hello();
 void ClearScreen();
 void Exit();
 
-
 int main()
 {
     char condition;
-do
-{
-    Hello();
-   Operation<int,int> op; //разобраться как создать шаблонный обьект
-
-std::cin >> condition;
-switch (condition)
+    do
     {
-    case '+':
-     std::cout << op.plus() << std::endl;
-    break;
-    case '-':
-    std::cout << op.minus() << std::endl;
-    break;
-    case '*':
-    std::cout << op.multi() << std::endl;
-    break;
-    case '/':
-    std::cout << op.divis() << std::endl;
-    break;
-    default:
-    std::cout << "Error input!" << std::endl;
-    break;
-    }
+        Hello();
+        Operation<int, int> op; // разобраться как создать шаблонный обьект
+
+        std::cin >> condition;
+        switch (condition)
+        {
+        case '+':
+            std::cout << op.plus() << std::endl;
+            break;
+        case '-':
+            std::cout << op.minus() << std::endl;
+            break;
+        case '*':
+            std::cout << op.multi() << std::endl;
+            break;
+        case '/':
+            std::cout << op.divis() << std::endl;
+            break;
+        default:
+            std::cout << "Error input!" << std::endl;
+            break;
+        }
+    } while (condition != '0');
 }
-while(condition != '0');
-
-
-
-
-}
-
-
-
-
 
 void Hello()
 {
     std::cout << "\t\tCalculator" << std::endl;
-
 }
 
 void ClearScreen()
