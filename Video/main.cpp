078a869bb528e5ff71256792e41b7cf987bb105d
DerMeister <v.savchenko2408@gmail.com>
2025-01-17 20:38:49 +0200

    clear Video

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
-#include <set>
+
 using std::cin;
 using std::cout;
 using std::endl;
-using std::set;
+
 
 int main()
 {
-  set<int> st;
-  st.insert(12);
-  st.insert(7);
-  st.insert(15);
-  st.insert(3);
-  st.insert(10);
-  st.emplace(5);
 
-
-  auto it = st.upper_bound(10);
-  cout << *it << endl;
   return 0;
 }
