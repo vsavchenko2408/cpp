f4ed30884996224aaa1c0047175dcb491d8bde84
DerMeister <v.savchenko2408@gmail.com>
2025-01-18 11:50:28 +0200

    functional

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
+#include <functional>
+#include <algorithm>
+#include <deque>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
+bool foo(int a, int b);
 
 int main()
 {
+  std::function<bool(int, int)> cmp;
+  cmp = foo;
+  std::deque<int> dq = {1,2,3,4,5,7,6,9,8,10};
+
+  std::sort(dq.begin(),dq.end(),cmp);
+
+  for(auto i: dq)
+  {
+    cout << i << endl;
+  }
+
+
+
 
   return 0;
+}
+
+
+bool foo(int a, int b)
+{
+  return a>b ? true : false ;
 }
