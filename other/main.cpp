7a3987e5760ad77920004ab5aeafa953df6bd9b2
DerMeister <v.savchenko2408@gmail.com>
2025-01-18 15:40:18 +0200

    clean Other

--- other/main.cpp
+++ other/main.cpp
@@ -1,23 +1,7 @@
 #include <iostream>
-#include <functional>
-#include <algorithm>
-#include <deque>
-
-template <typename T>
-bool foo(T a, T b)
-{
-    return a > b ? true : false;
-}
 
 int main()
 {
-    std::function<bool(double, double)> cmp = foo<double>;
-    std::deque<double> dq = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::sort(dq.begin(), dq.end(), cmp);
-    for (auto i : dq)
-    {
-        std::cout << i << std::endl;
-    }
 
     return 0;
 }
