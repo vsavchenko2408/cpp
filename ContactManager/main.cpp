20e832ffa1d0bfc065d3d53aeb6e25f8d2f313db
DerMeister <v.savchenko2408@gmail.com>
2025-01-23 10:58:18 +0200

    ContactManager

--- ContactManager/main.cpp
+++ ContactManager/main.cpp
@@ -1,16 +1,77 @@
 #include <iostream>
-
+#include <algorithm>
+#include <deque>
 using std::cin;
 using std::cout;
+using std::deque;
 using std::endl;
 using std::string;
 
 class Contact
 {
 private:
+    string name;
+    string number;
+    string email;
+
 public:
+    Contact() : name("empty"), number("empty"), email("empty") {}
+    Contact(string name) : Contact() {}
+    Contact(string name, string number, string email) : name(name), number(number), email(email) {}
+    bool operator==(const Contact &other) const
+    {
+        return name == other.name;
+    }
+    void show_info()
+    {
+        cout << "Name: " << '\t' << name << endl;
+        cout << "Number: " << '\t' << number << endl;
+        cout << "Email: " << '\t' << email << endl;
+    }
+    ~Contact() {}
 };
 
+bool search(string name, deque<Contact> dq)
+{
+    auto fnd = std::find(dq.begin(), dq.end(), name);
+    if (fnd != dq.end())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+Contact createContact()
+{
+    string name;
+    string number;
+    string email;
+    cout << "Enter name: ";
+    cin >> name;
+    cout << "Enter number: ";
+    cin >> number;
+    cout << "Enter email: ";
+    cin >> email;
+
+    Contact ct(name, number, email);
+    return ct;
+}
+Contact createContact(string name, string number, string email)
+{
+    Contact ct(name, number, email);
+    return ct;
+}
 int main()
 {
+    deque<Contact> contacts;
+    Contact Mira("Mira", "+380673398626", "empty");
+    contacts.push_back(Mira);
+    contacts.push_back(createContact());
+
+    for (auto &i : contacts)
+    {
+        i.show_info();
+    }
 }
