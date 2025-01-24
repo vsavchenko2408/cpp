8395789a2c411916c3527282b5f35ddc93c47c82
DerMeister <v.savchenko2408@gmail.com>
2025-01-24 09:34:59 +0200

    other

--- other/main.cpp
+++ other/main.cpp
@@ -4,24 +4,29 @@
 #include <random>
 
 using std::cout;
-using std::endl;
 using std::deque;
+using std::endl;
 
-
+bool cmp(int a)
+{
+    return a == 99;
+}
 int main()
 {
     srand(time_t(nullptr));
     deque<int> dq;
-    for(size_t i = 0; i < 10; ++i)
+    for (size_t i = 0; i < 50; ++i)
     {
-        dq.emplace_back(rand()%100);
+        dq.emplace_back(rand() % 100);
     }
 
-    std::sort(dq.begin(), dq.end(), [](int a, int b){return a<b? true : false;});
-
-    for(const auto& i : dq)
+    auto findif = std::for_each(dq.begin(), dq.end(), [](int &a)
+                                { a += 10; });
+    cout << findif << endl
+         << endl;
+    for (auto &i : dq)
     {
-        cout << i << endl;
+        cout << i << " ";
     }
     return 0;
     system("pause");
