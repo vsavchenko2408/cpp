bd9cbc18d1cc15e30aba3ade3ca88cb84a08c355
DerMeister <v.savchenko2408@gmail.com>
2025-01-24 21:04:17 +0200

    lambda2chapter

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
-
+#include <algorithm>
 using std::cin;
 using std::cout;
 using std::endl;
 
-// [](){}
-
 int main()
 {
-  auto mult = [](int a, int b) -> double
-  {
-    return a * b;
-  };
-  cout << mult(5.4, 5.2) << endl;
+
   return 0;
 }
