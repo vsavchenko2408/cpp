cfe36206eb9e65c39acd6b79bff469536b4e9893
DerMeister <v.savchenko2408@gmail.com>
2025-01-24 09:38:51 +0200

    other

--- other/main.cpp
+++ other/main.cpp
@@ -9,20 +9,19 @@ using std::endl;
 
 bool cmp(int a)
 {
-    return a == 99;
+    return a == 12;
 }
 int main()
 {
     srand(time_t(nullptr));
     deque<int> dq;
-    for (size_t i = 0; i < 50; ++i)
+    for (size_t i = 0; i < 10; ++i)
     {
-        dq.emplace_back(rand() % 100);
+        dq.emplace_back(rand() % 50);
     }
 
-    auto findif = std::for_each(dq.begin(), dq.end(), [](int &a)
-                                { a += 10; });
-    cout << findif << endl
+    auto findif = std::remove_if(dq.begin(), dq.end(), cmp);
+    cout << *findif << endl
          << endl;
     for (auto &i : dq)
     {
