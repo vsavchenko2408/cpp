6a4e403ba4eb4a907a9ea93e3259e9596d2dff45
DerMeister <v.savchenko2408@gmail.com>
2025-01-25 13:01:07 +0200

    count_if()

--- Video/main.cpp
+++ Video/main.cpp
@@ -7,16 +7,10 @@ using std::endl;
 
 int main()
 {
-  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  auto findif = std::find_if(arr, arr + 10, [](int &a)
-                             { return a >= 5; });
-  if (*(findif) != arr[10])
-  {
-    cout << "Find! " << findif - arr << endl;
-  }
-  else
-  {
-    cout << "Not find! " << endl;
-  }
+  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+  auto count = std::count_if(arr, arr + 14, [](int &a)
+                             { return (a % 2) == 0; });
+  cout << "Count: " << count << endl;
+
   return 0;
 }
