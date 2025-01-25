63e27ea05882b116aa92fa85c0ec6763d951d946
DerMeister <v.savchenko2408@gmail.com>
2025-01-25 12:55:46 +0200

    find_if()

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using std::cin;
 using std::cout;
@@ -6,6 +7,16 @@ using std::endl;
 
 int main()
 {
-
+  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  auto findif = std::find_if(arr, arr + 10, [](int &a)
+                             { return a >= 5; });
+  if (*(findif) != arr[10])
+  {
+    cout << "Find! " << findif - arr << endl;
+  }
+  else
+  {
+    cout << "Not find! " << endl;
+  }
   return 0;
 }
