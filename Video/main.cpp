ca9a50ec167bab4bbd45f81a57e7f59cf2b6d6f0
DerMeister <v.savchenko2408@gmail.com>
2025-01-25 13:22:33 +0200

    remove_if()

--- Video/main.cpp
+++ Video/main.cpp
@@ -8,9 +8,19 @@ using std::endl;
 int main()
 {
   int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-  auto count = std::count_if(arr, arr + 14, [](int &a)
-                             { return (a % 2) == 0; });
-  cout << "Count: " << count << endl;
 
+  auto end = std::remove_if(arr, arr + 14, [](int &a)
+                            { return (a % 2) == 0; });
+
+  int newarr[*(end)-1];
+  for (auto i = 0; i < *(end); ++i)
+  {
+    newarr[i] = arr[i];
+  }
+  for (auto &x : newarr)
+  {
+    cout << x << " ";
+  }
+  cout << endl;
   return 0;
 }
