f3e0605e697eff8a11642c1340a94414c6536b80
DerMeister <v.savchenko2408@gmail.com>
2025-02-14 19:41:17 +0200

    clear threads

--- threads/main.cpp
+++ threads/main.cpp
@@ -1,29 +1,7 @@
 #include <iostream>
-#include <thread>
-#include <chrono>
-void DoWork(bool work)
-{
-    while (work)
-    {
-        std::cout << "Working!" << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    }
-}
 
-void TenIter()
-{
-    for (size_t i = 0; i < 10; ++i)
-    {
-        std::cout << "Work " << i + 1 << "sec" << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    }
-}
 int main()
 {
-    std::thread th1(DoWork, true);
-    std::thread th2(TenIter);
 
-    th1.detach();
-    th2.join();
     return 0;
 }
