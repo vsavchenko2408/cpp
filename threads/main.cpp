90f5ca18d5aa36b915aa2f7510eb99971a18baad
DerMeister <v.savchenko2408@gmail.com>
2025-02-14 19:58:39 +0200

    threads created video lesson mutex lock and unlock

--- threads/main.cpp
+++ threads/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+std::mutex m;
+
+void show(char symbol)
+{
+    m.lock();
+    for (size_t i = 0; i < 10; ++i)
+    {
+        for (size_t j = 0; j < 10; ++j)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(25));
+            std::cout << symbol;
+        }
+        std::cout << std::endl;
+    }
+    m.unlock();
+}
 
 int main()
 {
+    std::thread th1(show, '*');
+    std::thread th2(show, '#');
+
+    th1.join();
+    th2.join();
 
     return 0;
 }
