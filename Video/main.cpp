5193b893f8f5e0f5dd77b7c6853b29b60b1faa42
DerMeister <v.savchenko2408@gmail.com>
2025-03-16 19:07:14 +0200

    clear video

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 
 using std::cin;
 using std::cout;
@@ -7,20 +6,6 @@ using std::endl;
 
 int main()
 {
-  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
-  auto end = std::remove_if(arr, arr + 14, [](int &a)
-                            { return (a % 2) == 0; });
-
-  int newarr[*(end)-1];
-  for (auto i = 0; i < *(end); ++i)
-  {
-    newarr[i] = arr[i];
-  }
-  for (auto &x : newarr)
-  {
-    cout << x << " ";
-  }
-  cout << endl;
   return 0;
 }
