efc425278c91ff8427c20fc0b7885fb24d62c236
DerMeister <v.savchenko2408@gmail.com>
2025-03-23 23:51:39 +0200

    SFMLtest

---
+++ SFMLtest/CMakeFiles/MyGame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyGame.dir/main.cpp.o"
+  "CMakeFiles/MyGame.dir/main.cpp.o.d"
+  "MyGame"
+  "MyGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
