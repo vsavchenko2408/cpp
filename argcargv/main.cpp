5d92cbcce380d007414609c122a32830dc92ae3e
DerMeister <v.savchenko2408@gmail.com>
2025-04-06 21:25:24 +0300

    argcargv video

--- argcargv/main.cpp
+++ argcargv/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
-
+    for (size_t i = 0; i < argc; ++i)
+    {
+        std::cout << "Param: " << i << " " << argv[i] << std::endl;
+    }
+    std::cout << "Program sucessful!" << std::endl;
     return 0;
 }
