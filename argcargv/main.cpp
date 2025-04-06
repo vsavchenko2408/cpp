90ff56b96160fe65bf30b51db772350c3fa28c38
DerMeister <v.savchenko2408@gmail.com>
2025-04-06 20:53:22 +0300

    clear argcargv

--- argcargv/main.cpp
+++ argcargv/main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 
-int main(int argc, char *argv[]) // argc = size, argv = parameters
+int main()
 {
-    for (size_t i = 0; i < argc; ++i)
-    {
-        std::cout << i << ": " << argv[i] << std::endl;
-    }
 
     return 0;
 }
