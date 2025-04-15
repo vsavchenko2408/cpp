91a22759ead9a71f71fa7941186e15aba1d044fa
DerMeister <v.savchenko2408@gmail.com>
2025-04-15 22:11:50 +0300

    video rules of three

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,11 +1,48 @@
+// Правило нуля
+// Правило трьох
+// Правило п'яти
+
 #include <iostream>
+#include <vector>
+class MyClass
+{
+  int *data;
 
-using std::cin;
-using std::cout;
-using std::endl;
+public:
+  MyClass(int d)
+  {
+    data = new int(d);
+  };
 
+  ~MyClass() // 1
+  {
+    delete data;
+    data = nullptr;
+  }
+
+  MyClass(const MyClass &obj)
+  {
+    data = new int(*obj.data);
+  }
+  MyClass &operator=(const MyClass &obj)
+  {
+    if (this != &obj)
+    {
+      delete data;
+      data = new int(*obj.data);
+    }
+    return *this;
+  }
+  void show()
+  {
+    std::cout << "data: " << *data << std::endl;
+  }
+};
 int main()
 {
-
+  MyClass a(15);
+  MyClass b(a);
+  //a.~MyClass();
+  b.show();
   return 0;
 }
