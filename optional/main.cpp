85055ebc5e1ec805c50604bc94a22f356ef6e8b7
DerMeister <v.savchenko2408@gmail.com>
2025-06-28 20:06:12 +0300

    video std::optional

--- optional/main.cpp
+++ optional/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <optional>
+
+std::optional<int> find(int *arr, const int SIZE, int target)
+{
+    for (int i = 0; i < SIZE; ++i)
+    {
+        if (target == arr[i])
+        {
+            return arr[i];
+        }
+    }
+    return std::nullopt;
+}
 
 int main()
 {
+    int a = 5;
+
+    const int SIZE = 10;
+    int arr[SIZE] = {0, -1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::optional op = find(arr, SIZE, 10);
+    if (op.has_value())
+    {
+        std::cout << "Find: " << op.value() << std::endl;
+    }
+    else
+    {
+        std::cout << "Value not found!" << std::endl;
+    }
 }
