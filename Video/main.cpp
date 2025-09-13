2708b6fc642412898dbdd2b70dca97e1c7cddb59
DerMeister <v.savchenko2408@gmail.com>
2025-09-13 23:40:44 +0200

    Video rule of five

--- Video/main.cpp
+++ Video/main.cpp
@@ -3,9 +3,61 @@
 // Правило п'яти
 
 #include <iostream>
+class Data
+{
+private:
+  int *m_data;
 
+public:
+  Data() : m_data(new int(0))
+  {
+  }
+  Data(int data) : m_data(new int(data))
+  {
+  }
+  ~Data()
+  {
+    std::cout << "destructor" << std::endl;
+    delete m_data;
+  }
+  Data(const Data &obj) : m_data(new int(*obj.m_data))
+  {
+    std::cout << "constr copy" << std::endl;
+  }
+  Data &operator=(const Data &obj)
+  {
+    std::cout << "Operator copy" << std::endl;
+    if (this != &obj)
+    {
+      delete m_data;
+      m_data = new int(*obj.m_data);
+    }
+
+    return *this;
+  }
+
+  Data(Data &&obj) noexcept : m_data(obj.m_data)
+  {
+    std::cout << "Move constr" << std::endl;
+    obj.m_data = nullptr;
+  }
+  Data &operator=(Data &&obj) noexcept
+  {
+    std::cout << "Move operator" << std::endl;
+    if (this != &obj)
+    {
+      delete m_data;
+      m_data = obj.m_data;
+      obj.m_data = nullptr;
+    }
+    return *this;
+  }
+};
 int main()
 {
+  Data a(15);
+  Data b;
+  b = std::move(a);
 
   return 0;
 }
