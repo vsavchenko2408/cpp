baddca0c85ce7a2afda5ab920266602185349c76
DerMeister <v.savchenko2408@gmail.com>
2025-09-14 20:15:23 +0200

    Video rule 0 3 5

--- Video/main.cpp
+++ Video/main.cpp
@@ -1,11 +1,67 @@
 // Правило нуля
-// Правило трьох
-// Правило п'яти
+// Правило трьох c++ 11
+// конструктор копіювання
+// оператор копіювання
+// деструктор
+
+// Правило п'яти after C++ 11
+// конструктор копіювання
+// оператор копіювання
+// конструктор переміщення
+// оператор переміщення
+// деструктор
 
 #include <iostream>
+class Data
+{
+private:
+  int *m_data;
 
+public:
+  Data() : m_data(nullptr) {}
+  Data(int data) : m_data(new int(data)) {}
+  Data(Data &copy) : m_data(new int(*copy.m_data)) { std::cout << __PRETTY_FUNCTION__ << std::endl; } // copy constr
+  Data operator=(const Data &copy)                                                                    // copy operator
+  {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    if (this != &copy)
+    {
+      delete m_data;
+      m_data = copy.m_data;
+    }
+    return *this;
+  }
+  Data(Data &&move) noexcept : m_data(move.m_data) // move constr
+  {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    move.m_data = nullptr;
+  }
+  Data &operator=(Data &&move) noexcept // move operator
+  {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    if (this != &move)
+    {
+      m_data = nullptr;
+      m_data = new int(*move.m_data);
+      move.m_data = nullptr;
+    }
+    return *this;
+  }
+  int get_data()
+  {
+    return *m_data;
+  }
+  ~Data() // destructor
+  {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    delete m_data;
+  }
+};
 int main()
 {
-
+  Data a(15);
+  Data b(a);
+  Data c(std::move(a));
+  c.get_data();
   return 0;
 }
