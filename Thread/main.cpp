27988bb301a47087e0de15ae7d5f460dc32867da
DerMeister <v.savchenko2408@gmail.com>
2025-09-24 10:07:49 +0200

    thread

---
+++ Thread/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <thread>
+
+class Thread
+{
+    private:
+        std::thread t;
+    public:
+        Thread()
+        {
+            
+        }
+};
+int main()
+{
+    
+}
