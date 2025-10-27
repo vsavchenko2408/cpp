ade16f0a756b51e54b8ebc43772d3cc1b914bcee
P15s <v.savchenko2408@gmail.com>
2025-10-27 18:50:36 +0100

    arraycont

---
+++ arraycont/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <random>
+int count_arr(int* arr, const int SIZE)
+{
+    int value = 0;
+for(int i = 0;i<SIZE;i++)
+    {
+        value += arr[i];
+    }
+    return value;
+}
+void fill_arr(int* arr, const int SIZE)
+{
+    for(int i = 0;i< SIZE;i++)
+    {
+        arr[i] = rand()%10;
+    }
+}
+void show_arr(int* arr, const int SIZE)
+{
+    for(int i = 0; i < SIZE; i++)
+    {
+        std::cout << i+1 << ": " << arr[i] << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    const int SIZE = 10;
+    int* parr = new int[SIZE];
+    srand(time(NULL));
+
+    fill_arr(parr, SIZE);
+    show_arr(parr, SIZE);
+    int summ = count_arr(parr, SIZE);
+    std::cout << "Summ of array = " << summ << std::endl;
+
+
+    delete parr;
+
+    return 0;
+}
