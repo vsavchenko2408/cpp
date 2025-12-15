111c735a0d494ed09e16e9c73ba75195bb176e5e
Der Meister <v.savchenko2408@gmail.com>
2025-12-16 00:28:38 +0100

    string

--- string/main.cpp
+++ string/main.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
-#include <string>
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
+
 
 int main()
 {
 
-string str("some text");
-cout << str.capacity() << endl;
-str.reserve(100);
 
-str.shrink_to_fit();
-cout << str.capacity() << endl;
-//cout << str;
-cout << endl;
 return 0;
 }
