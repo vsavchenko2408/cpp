a05350d32a705ae89a157cf26245fb5a5fec0140
Der Meister <v.savchenko2408@gmail.com>
2026-02-17 22:16:29 +0100

    clean Smartpointers

--- Smartpointers/main.cpp
+++ Smartpointers/main.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
-#include "smartpointers.h"
+
 
 
 int main()
 {
-    int* ptr_a = new int(5); 
-    
-SmartPointer<int> ptr(ptr_a);
 
-SmartPointer<int> ptr1(ptr_a);
-
-std::cout << (ptr<ptr1) << std::endl;
 
     return 0;
 }
